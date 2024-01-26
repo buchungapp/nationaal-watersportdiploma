23924c0159603014e10a6ccc7c645a16cebb020e
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-01-26 16:26:37 +0100

    Adds drizzle (#5)
    * adds db package
    
    * nwd-db project with drizzle
    
    * using drizzle
    
    * some comments
    
    * move settings to workspace
    
    * moves extensions to workspace
    
    * update lock
    
    * use parseAsync
    
    * call it configuration
    
    * adds version and demandcommand
    
    * remove shebang

---
+++ packages/nwd-db/migrations/0000_strong_skreet.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS "echo_messages" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"message_value" text NOT NULL
+);
---
+++ packages/nwd-db/src/testing/database.test.ts
@@ -0,0 +1,10 @@
+import assert from "assert";
+import { sql } from "drizzle-orm";
+import test from "node:test";
+import { withDatabase } from "./database.js";
+
+test("with-database", () =>
+  withDatabase(async ({ db }) => {
+    const result = await db.execute(sql`SELECT 1 as one;`);
+    assert.deepEqual(result.rows, [{ one: 1 }]);
+  }));
