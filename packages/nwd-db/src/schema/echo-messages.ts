aefe471c0b5d3df0f539a1a91a7f9f455e731b7a
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-01-26 12:39:27 +0100

    drop table

--- packages/nwd-db/src/schema/echo-messages.ts
+++
@@ -1,6 +0,0 @@
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
-
-export const echoMessages = pgTable("echo_messages", {
-  id: serial("id").primaryKey(),
-  messageValue: text("message_value").notNull(),
-});
--- packages/nwd-db/src/schema/index.ts
+++ packages/nwd-db/src/schema/index.ts
@@ -1,3 +1,2 @@
-export * from "./echo-messages.js";
 export * from "./main-category.js";
 export * from "./sub-category.js";
