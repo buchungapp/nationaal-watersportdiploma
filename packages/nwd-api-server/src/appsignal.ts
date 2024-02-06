2a2ef8149b03e30d4c9fddfa41db604741633719
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-02-06 13:54:09 +0100

    load appsignal first

--- packages/nwd-api-server/src/appsignal.ts
+++ packages/nwd-api-server/src/appsignal.ts
@@ -2,7 +2,7 @@ import { Appsignal } from "@appsignal/nodejs";
 
 const appsignalPushApiKey = process.env.APPSIGNAL_PUSH_API_KEY;
 
-const appsignal = new Appsignal({
+export const appsignal = new Appsignal({
   active: appsignalPushApiKey != null,
   environment: process.env.NODE_ENV,
   name: "nwd-api-server",
