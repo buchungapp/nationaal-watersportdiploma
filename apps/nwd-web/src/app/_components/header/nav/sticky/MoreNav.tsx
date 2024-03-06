bbca2745f060aed4a168109c3309d80920b8d2f1
Thomas B <t.ferb1@gmail.com>
2024-03-06 12:57:18 +0100

    fix: nav more hover

--- apps/nwd-web/src/app/_components/header/nav/sticky/MoreNav.tsx
+++ apps/nwd-web/src/app/_components/header/nav/sticky/MoreNav.tsx
@@ -6,6 +6,7 @@ import { Menu, Transition } from "@headlessui/react";
 import clsx from "clsx";
 import Link from "next/link";
 
+import Double from "~/app/_assets/Double";
 import { type NavItem } from "../Nav";
 
 export default function MoreNav({ items }: { items: NavItem[] }) {
@@ -14,13 +15,16 @@ export default function MoreNav({ items }: { items: NavItem[] }) {
       {({ open }) => (
         <>
           <Menu.Button
-            className={clsx(
-              open ? "border-branding-dark" : "border-transparent hover:border-branding-dark",
-              "relative z-10 flex flex-nowrap items-center gap-1 border-b-2 py-0.5 uppercase transition-colors",
-            )}
+            className={"relative z-10 flex flex-nowrap items-center gap-1 py-0.5 uppercase group"}
           >
             <span>Overig</span>
-            {/* <ChevronDownIcon className="h-6 w-6" aria-hidden="true" /> */}
+            {/* <ChevronDownIcon className="h-6 w-6" aria-hidden="true" /> */}{" "}
+            <Double
+              className={clsx(
+                "w-full bottom-0 translate-y-full absolute transition-colors",
+                open ? "text-branding-dark" : "group-hover:text-branding-dark text-transparent",
+              )}
+            />
           </Menu.Button>
           <Transition
             as={Fragment}
