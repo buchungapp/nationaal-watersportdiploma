625f67e61b25dcb44debfccb9cdee01081cf270a
Thomas B <t.ferb1@gmail.com>
2024-03-09 20:31:42 +0100

    fix: small padding changes in mobile nav

--- apps/nwd-web/src/app/_components/header/nav/mobile/MobileNavItems.tsx
+++ apps/nwd-web/src/app/_components/header/nav/mobile/MobileNavItems.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 
+import clsx from "clsx";
 import { NavItem, type NavItems } from "../Nav";
 
 export default function MobileNavItems({ items }: { items: NavItems }) {
@@ -7,29 +8,27 @@ export default function MobileNavItems({ items }: { items: NavItems }) {
     <div className="p-4 overflow-y-auto scrollbar-none divide-y-2 divide-slate-100">
       {items.map((item) =>
         "items" in item ? (
-          <div key={item.label}>
-            <div className="flex gap-1 px-4 pt-4 font-semibold text-branding-dark">
-              {item.label}
-            </div>
+          <div key={item.label} className="gap-2 flex flex-col py-4">
+            <div className="flex gap-1 px-4 font-semibold">{item.label}</div>
             <div className="text-sm">
               {item.items.map((subItem) => (
-                <Item key={subItem.label} item={subItem} />
+                <Item key={subItem.label} item={subItem} className="px-4 py-0.5" />
               ))}
             </div>
           </div>
         ) : (
-          <Item key={item.label} item={item} />
+          <Item key={item.label} item={item} className="p-4" />
         ),
       )}
     </div>
   );
 }
 
-function Item({ item }: { item: NavItem }) {
+function Item({ item, className }: { item: NavItem; className?: string }) {
   return (
     <Link
       href={item.href}
-      className="relative block p-4 transition-colors hover:bg-branding-section"
+      className={clsx("relative block transition-colors hover:bg-branding-section", className)}
     >
       <div className="flex gap-1 font-semibold text-branding-dark">
         {item.icon}
