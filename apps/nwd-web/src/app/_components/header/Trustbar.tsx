287e11ce2a9b195b11d7adc9694e17f4a985f3d4
Maurits Misana <maurits@techmaus.nl>
2024-03-10 12:09:57 +0100

    Style social icons in trust bar, and move constants to new lib package

--- apps/nwd-web/src/app/_components/header/Trustbar.tsx
+++ apps/nwd-web/src/app/_components/header/Trustbar.tsx
@@ -1,64 +1,65 @@
 import Link from "next/link";
-import Facebook from "~/app/_assets/social/Facebook";
-import Instagram from "~/app/_assets/social/Instagram";
-import Linkedin from "~/app/_assets/social/Linkedin";
-import Tiktok from "~/app/_assets/social/Tiktok";
-import Youtube from "~/app/_assets/social/Youtube";
+import {
+  APP_SLOGAN,
+  FACEBOOK_URL,
+  INSTAGRAM_URL,
+  LINKEDIN_URL,
+  TIKTOK_URL,
+  YOUTUBE_URL,
+} from "nwd-lib/constants";
+import { Facebook, Instagram, LinkedIn, TikTok, YouTube } from "../socials";
 
-export default async function Trustbar() {
+export default function Trustbar() {
+  const socials = [
+    {
+      name: "Facebook",
+      icon: Facebook,
+      link: FACEBOOK_URL,
+    },
+    {
+      name: "Instagram",
+      icon: Instagram,
+      link: INSTAGRAM_URL,
+    },
+    {
+      name: "LinkedIn",
+      icon: LinkedIn,
+      link: LINKEDIN_URL,
+    },
+    {
+      name: "TikTok",
+      icon: TikTok,
+      link: TIKTOK_URL,
+    },
+    {
+      name: "YouTube",
+      icon: YouTube,
+      link: YOUTUBE_URL,
+    },
+  ];
+
   return (
     <section className="text-white py-2 sm:px-28 flex items-center justify-center lg:justify-between gap-2">
       <div className="xl:block hidden flex-1"></div>
       <p className="xl:text-center lg:text-start text-center flex-1 text-sm font-semibold uppercase">
-        veiligheid, kwaliteit & plezier op het water.
+        {APP_SLOGAN}
       </p>
 
       <ul className="items-center flex-1 gap-6 justify-end lg:flex hidden">
-        <li>
-          <Link
-            href={`https://facebook.com/nationaalwatersportdiploma`}
-            target="_blank"
-            referrerPolicy="no-referrer"
-          >
-            <Facebook className="h-4 w-4" />
-          </Link>
-        </li>
-        <li>
-          <Link
-            href={`https://www.instagram.com/nationaalwatersportdiploma`}
-            target="_blank"
-            referrerPolicy="no-referrer"
-          >
-            <Instagram className="h-4 w-4" />
-          </Link>
-        </li>
-        <li>
-          <Link
-            href={`https://www.linkedin.com/company/nationaal-watersportdiploma/`}
-            target="_blank"
-            referrerPolicy="no-referrer"
-          >
-            <Linkedin className="h-4 w-4" />
-          </Link>
-        </li>
-        <li>
-          <Link
-            href={`https://www.tiktok.com/@nwdiploma`}
-            target="_blank"
-            referrerPolicy="no-referrer"
-          >
-            <Tiktok className="h-4 w-4" />
-          </Link>
-        </li>
-        <li>
-          <Link
-            href={`https://www.youtube.com/@nationaalwatersportdiploma`}
-            target="_blank"
-            referrerPolicy="no-referrer"
-          >
-            <Youtube className="h-4 w-4" />
-          </Link>
-        </li>
+        {socials.map((social, i) => (
+          <li key={i}>
+            <Link
+              className="relative group"
+              href={social.link}
+              target="_blank"
+              referrerPolicy="no-referrer"
+            >
+              {/* Background on hover */}
+              <div className="absolute inset-0 scale-150 bg-white rounded bg-opacity-0 group-hover:bg-opacity-100 transition-opacity" />
+              <social.icon className="relative h-4 w-4 group-hover:text-[--brand-color]" />
+            </Link>
+          </li>
+        ))}
       </ul>
     </section>
   );
