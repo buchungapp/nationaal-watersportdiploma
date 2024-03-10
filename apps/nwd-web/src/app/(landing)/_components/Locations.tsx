c8a099b520ee2ae280a73721be85fe65271f2cf2
Maurits Misana <maurits@techmaus.nl>
2024-03-10 17:19:31 +0100

    Update locations section homepage

--- apps/nwd-web/src/app/(landing)/_components/Locations.tsx
+++ apps/nwd-web/src/app/(landing)/_components/Locations.tsx
@@ -1,30 +1,34 @@
 import Image from "next/image";
 
-import Article from "~/app/_components/style/Article";
+import Double from "~/app/_components/brand/double-line";
 import { BoxedButton } from "~/app/_components/style/Buttons";
 import locations from "./_assets/locations.png";
 
 export default function Locations() {
   return (
     <section className="w-full bg-branding-orange rounded-[3rem] grid items-center lg:grid-cols-2 overflow-hidden">
-      <Article className="px-4 py-12 lg:p-16 text-white">
-        <Article.Heading>Locaties</Article.Heading>
-        <Article.Title>Vind jouw NWD-vaarlocatie.</Article.Title>
-        <Article.Paragraph>
+      <div className="px-4 py-12 lg:p-16 text-white">
+        <div className="flex font-bold uppercase items-center gap-x-3 text-white">
+          Locaties
+          <Double />
+        </div>
+        <h3 className="text-white mt-1.5 font-bold text-2xl">Vind jouw NWD-vaarlocatie.</h3>
+        <p className="text-orange-100 mt-2.5">
           NWD-locaties voldoen aan strenge kwaliteitseisen op gebied van persoonlijke- en sociale
           veiligheid. Verder hebben zij zich gecommitteerd aan de visie van het NWD: veiligheid,
           kwaliteit en plezier op het water. Daarom zijn zij gelicenseerd om het NWD-diploma uit te
           geven.
-        </Article.Paragraph>
-        <Article.ButtonSection className="mt-8">
+        </p>
+
+        <div className="mt-8 flex items-center gap-x-6">
           <BoxedButton href="/locaties" className="bg-white text-slate-900">
             Bekijk NWD locaties
           </BoxedButton>
-          <BoxedButton href="/kwaliteitseisen-locaties" className="bg-branding-orange text-white">
+          <BoxedButton href="/kwaliteitseisen-locaties" className="hover:bg-white/10 text-white">
             Kwaliteitseisen voor locaties
           </BoxedButton>
-        </Article.ButtonSection>
-      </Article>
+        </div>
+      </div>
       <Image
         src={locations}
         alt="NWD-locaties"
