4c27091939f5a6d651d2f08e4befd8670bd608fd
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-03-11 16:31:00 +0100

    replace make

---
+++ scripts/install.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+const cp = require("child_process");
+const path = require("path");
+
+const options = { shell: true, stdio: "inherit" };
+
+cp.execFileSync(
+  "pnpm",
+  [
+    "--package",
+    "oa42-generator@0.9.6",
+    "dlx",
+    "oa42-generator",
+    "package",
+    path.resolve("specifications", "api.yaml"),
+    "--package-directory",
+    path.resolve("generated", "api"),
+    "--package-name",
+    "@nawadi/api",
+    "--package-version",
+    "0.0.0",
+  ],
+  options,
+);
+
+cp.execFileSync("pnpm", ["--filter", "@nawadi/api", "install", "--no-lockfile"], options);
+cp.execFileSync("pnpm", ["--filter", "@nawadi/api", "build"], options);
