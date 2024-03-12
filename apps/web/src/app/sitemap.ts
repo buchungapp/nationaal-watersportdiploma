303c17bbcef421f3ace5ce0e89e6d5fdc2490476
Thomas B <t.ferb1@gmail.com>
2024-03-12 11:08:53 +0100

    feat: sitemap

---
+++ apps/web/src/app/sitemap.ts
@@ -0,0 +1,102 @@
+import { type MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const BASE_URL = "https://www.nationaalwatersportdiploma.nl";
+
+  return [
+    {
+      url: `${BASE_URL}`,
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+
+    {
+      url: `${BASE_URL}/vaarlocaties`,
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${BASE_URL}/over`,
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${BASE_URL}/faq`,
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${BASE_URL}/contact`,
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${BASE_URL}/actueel`,
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+
+    {
+      url: `${BASE_URL}/diplomalijn`,
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${BASE_URL}/diplomalijn/consumenten`,
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${BASE_URL}/diplomalijn/instructeurs`,
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${BASE_URL}/diplomalijn/accreditatie`,
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+
+    {
+      url: `${BASE_URL}/vereniging/manifest`,
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${BASE_URL}/vereniging/vertrouwenspersoon`,
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${BASE_URL}/vereniging/gedragscode`,
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${BASE_URL}/vereniging/bestuur`,
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${BASE_URL}/vereniging/secretariaat`,
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${BASE_URL}/vereniging/kwaliteitscommissie`,
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${BASE_URL}/vereniging/statuten-en-reglementen`,
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+
+    {
+      url: `${BASE_URL}/mediakit`,
+      changeFrequency: "monthly",
+      priority: 0.6,
+    },
+  ];
+}
