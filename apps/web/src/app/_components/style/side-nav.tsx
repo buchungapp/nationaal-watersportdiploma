988c2136b60bca891d8ee2960fb70e7c7d53b95a
Thomas B <t.ferb1@gmail.com>
2024-03-17 12:58:44 +0100

    feat: active side-nav for url params

--- apps/web/src/app/_components/style/side-nav.tsx
+++ apps/web/src/app/_components/style/side-nav.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import Link from "next/link";
-import { usePathname } from "next/navigation";
 import clsx from "clsx";
+import Link from "next/link";
+import { usePathname, useSearchParams } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 
 export default function SideNav({
@@ -15,26 +15,42 @@ export default function SideNav({
   className?: string;
 }) {
   const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const searchParamsList = Array.from(searchParams.entries());
 
   return (
     <div className={twMerge("flex flex-col gap-2 text-sm", className)}>
       <span className="ml-4 text-sm font-semibold">{label}</span>
       <ul className="flex flex-col gap-3">
-        {items.map(({ href, label }) => (
-          <li key={href}>
-            <Link
-              href={href}
-              className={clsx(
-                "block rounded-lg px-4 py-1.5 text-branding-dark transition-colors",
-                pathname === href
-                  ? "bg-gray-100 font-semibold"
-                  : "hover:bg-gray-100",
-              )}
-            >
-              {label}
-            </Link>
-          </li>
-        ))}
+        {items.map(({ href, label }) => {
+          const url = new URL(href, "http://localhost");
+          const currentPathname = url.pathname;
+          const currentSearchParams = url.searchParams;
+          const currentSearchParamsList = Array.from(
+            currentSearchParams.entries(),
+          );
+
+          const matchesPathname = pathname === currentPathname;
+          const matchesSearchParams = currentSearchParamsList.every(
+            ([key, value]) =>
+              searchParamsList.some(([k, v]) => k === key && v === value),
+          );
+          const isActive = matchesPathname && matchesSearchParams;
+
+          return (
+            <li key={href}>
+              <Link
+                href={href}
+                className={clsx(
+                  "block rounded-lg px-4 py-1.5 text-branding-dark transition-colors",
+                  isActive ? "bg-gray-100 font-semibold" : "hover:bg-gray-100",
+                )}
+              >
+                {label}
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
