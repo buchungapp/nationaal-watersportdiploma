9011fb25198703f4bd6f5731b6f4e91e61fc0701
Maurits Misana <maurits@techmaus.nl>
2024-03-18 09:23:02 +0100

    Temporarily disable active menu item

--- apps/web/src/app/_components/header/nav/sticky/active-hover.tsx
+++ apps/web/src/app/_components/header/nav/sticky/active-hover.tsx
@@ -1,17 +1,17 @@
 "use client";
 
 import clsx from "clsx";
-import { usePathname } from "next/navigation";
+import { useSelectedLayoutSegment } from "next/navigation";
 import Double from "~/app/_components/brand/double-line";
 
 export default function ActiveHover({ active }: { active?: string }) {
-  const pathname = usePathname();
+  const segment = useSelectedLayoutSegment();
 
   return (
     <Double
       className={clsx(
         "absolute bottom-0 w-0 translate-y-full text-branding-dark transition-width",
-        active && pathname.startsWith(active)
+        active && segment && active.includes(segment)
           ? "w-full"
           : "group-hover:w-full ui-open:w-full",
       )}
--- apps/web/src/app/_components/style/side-nav.tsx
+++ apps/web/src/app/_components/style/side-nav.tsx
@@ -1,17 +1,16 @@
 "use client";
 
-import { XMarkIcon } from "@heroicons/react/24/outline";
 import clsx from "clsx";
 import Link from "next/link";
-import { usePathname, useSearchParams } from "next/navigation";
+// import { usePathname, useSearchParams } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 
 export default function SideNav({
   label,
   items,
   className,
-  clear,
-  params = false,
+  clear: _c,
+  params: _p,
 }: {
   label: string;
   items: { label: string; href: string }[];
@@ -19,41 +18,41 @@ export default function SideNav({
   clear?: string;
   params?: boolean;
 }) {
-  const pathname = usePathname();
+  // const pathname = usePathname();
 
-  let active: { active: boolean; href: string }[] = [];
-  if (params) {
-    const searchParams = useSearchParams();
-    const searchParamsList = Array.from(searchParams.entries());
-    active = items.map(({ href }) => {
-      const url = new URL(href, "http://localhost");
-      const currentPathname = url.pathname;
-      const currentSearchParams = url.searchParams;
-      const currentSearchParamsList = Array.from(currentSearchParams.entries());
+  // let active: { active: boolean; href: string }[] = [];
+  // if (params) {
+  //   const searchParams = useSearchParams();
+  //   const searchParamsList = Array.from(searchParams.entries());
+  //   active = items.map(({ href }) => {
+  //     const url = new URL(href, "http://localhost");
+  //     const currentPathname = url.pathname;
+  //     const currentSearchParams = url.searchParams;
+  //     const currentSearchParamsList = Array.from(currentSearchParams.entries());
 
-      const matchesPathname = pathname === currentPathname;
-      const matchesSearchParams = currentSearchParamsList.every(
-        ([key, value]) =>
-          searchParamsList.some(([k, v]) => k === key && v === value),
-      );
+  //     const matchesPathname = pathname === currentPathname;
+  //     const matchesSearchParams = currentSearchParamsList.every(
+  //       ([key, value]) =>
+  //         searchParamsList.some(([k, v]) => k === key && v === value),
+  //     );
 
-      return { href, active: matchesPathname && matchesSearchParams };
-    });
-  } else {
-    active = items.map(({ href }) => {
-      const url = new URL(href, "http://localhost");
-      const currentPathname = url.pathname;
-      const matchesPathname = pathname === currentPathname;
+  //     return { href, active: matchesPathname && matchesSearchParams };
+  //   });
+  // } else {
+  //   active = items.map(({ href }) => {
+  //     const url = new URL(href, "http://localhost");
+  //     const currentPathname = url.pathname;
+  //     const matchesPathname = pathname === currentPathname;
 
-      return { href, active: matchesPathname };
-    });
-  }
+  //     return { href, active: matchesPathname };
+  //   });
+  // }
 
   return (
     <div className={twMerge("flex flex-col gap-2 text-sm", className)}>
       <div className="flex pl-4 justify-between gap-1">
         <span className="text-sm font-semibold">{label}</span>
-        {clear && active.some((x) => x.active) ? (
+        {/* {clear && active.some((x) => x.active) ? (
           <Link
             href={clear}
             className={
@@ -62,19 +61,19 @@ export default function SideNav({
           >
             <XMarkIcon className="w-4 h-4" />
           </Link>
-        ) : null}
+        ) : null} */}
       </div>
       <ul className="flex flex-col gap-3">
         {items.map(({ href, label }) => {
-          const isActive = active.find((item) => item.href === href)?.active;
+          // const isActive = active.find((item) => item.href === href)?.active;
 
           return (
             <li key={href}>
               <Link
                 href={href}
                 className={clsx(
-                  "block rounded-lg px-4 py-1.5 text-branding-dark transition-colors",
-                  isActive ? "bg-gray-100 font-semibold" : "hover:bg-gray-100",
+                  "block rounded-lg px-4 py-1.5 text-branding-dark transition-colors hover:bg-gray-100",
+                  // isActive ? "bg-gray-100 font-semibold" : "hover:bg-gray-100",
                 )}
               >
                 {label}
