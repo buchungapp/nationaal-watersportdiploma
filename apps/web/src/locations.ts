d237920bebaad5a004318bab71dab16bfcd56315
Maurits Misana <maurits@techmaus.nl>
2024-03-21 17:48:05 +0100

    Add page locations

---
+++ apps/web/src/locations.ts
@@ -0,0 +1,56 @@
+export const locations = [
+  {
+    id: 1,
+    name: "Zeilschool De Veenhoop",
+    position: { lat: 53.0956873, lng: 5.9414746 },
+    url: "https://zeilschoolfriesland.nl/",
+  },
+  {
+    id: 2,
+    name: "Zeilschool de Kikkert",
+    position: { lat: 52.8719666, lng: 5.640954 },
+    url: "https://www.dekikkert.nl/",
+  },
+  {
+    id: 3,
+    name: "Zeilschool Het Molenhuis",
+    position: { lat: 52.9420337, lng: 5.6265536 },
+    url: "https://zeilschoolhetmolenhuis.com/",
+  },
+  {
+    id: 4,
+    name: "Zeilschool Wavie",
+    position: { lat: 52.0013379, lng: 4.5502541 },
+    url: "https://www.wavie.nl/",
+  },
+  {
+    id: 5,
+    name: "Krekt Sailing",
+    position: { lat: 52.9948167, lng: 5.7144898 },
+    url: "https://www.krektsailing.nl/",
+  },
+  {
+    id: 6,
+    name: "Zeil- en Surfschool Neptunus",
+    position: { lat: 52.9294325, lng: 5.7085266 },
+    url: "https://www.zeilschoolneptunus.nl/",
+  },
+  {
+    id: 7,
+    name: "Zeilschool Aalsmeer",
+    position: { lat: 52.2504869, lng: 4.7556987 },
+    url: "https://www.zeilschoolaalsmeer.nl/",
+  },
+  {
+    id: 8,
+    name: "Zeilschool Pean",
+    position: { lat: 53.08001, lng: 5.8588931 },
+    url: "https://www.pean.nl/",
+  },
+  {
+    id: 9,
+    name: "Zeilschool Vinkeveen",
+    position: { lat: 52.2176868, lng: 4.9307842 },
+    url: "https://www.vinkeveenhaven.nl/pages/zeilschool",
+  },
+] as const;
