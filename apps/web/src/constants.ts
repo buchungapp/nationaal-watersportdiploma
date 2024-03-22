52eb5ba9c7982c7d7f7cd89761497d36f9182d84
Maurits <maurits@techmaus.nl>
2024-03-22 07:42:01 +0100

    Prefix env variables for base url with NEXT_PUBLIC so they work client side (#91)

--- apps/web/src/constants.ts
+++ apps/web/src/constants.ts
@@ -1,10 +1,8 @@
 import { WEBSITE_URL } from "@nawadi/lib/constants";
 
 export const BASE_URL =
-  process.env.VERCEL_ENV === "production"
+  process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
     ? new URL(WEBSITE_URL)
-    : process.env.VERCEL_URL
-      ? new URL(`https://${process.env.VERCEL_URL}`)
-      : process.env.VERCEL_BRANCH_URL
-        ? new URL(`https://${process.env.VERCEL_BRANCH_URL}`)
-        : new URL(`http://localhost:${process.env.PORT ?? 3000}`);
+    : process.env.NEXT_PUBLIC_VERCEL_URL
+      ? new URL(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}`)
+      : new URL(`http://localhost:${process.env.PORT ?? 3000}`);
