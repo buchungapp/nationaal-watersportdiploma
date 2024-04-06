9ffdf2d0ce17783e2d6061d3c6b207e70a277edb
Maurits Misana <maurits@techmaus.nl>
2024-04-06 21:36:45 +0200

    Add  blur placeholder to login page

--- apps/web/src/app/(dashboard)/login/layout.tsx
+++ apps/web/src/app/(dashboard)/login/layout.tsx
@@ -14,6 +14,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             className="absolute inset-0 h-full w-full object-cover"
             priority
             src={coverImage}
+            placeholder="blur"
             alt=""
           />
         </div>
