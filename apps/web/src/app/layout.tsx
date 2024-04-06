cd9ed35f804f8ed681ee8365c5ad783dd0fe4153
Maurits Misana <maurits@techmaus.nl>
2024-04-06 17:17:48 +0200

    more setup

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,60 @@
+import { clsx } from "clsx";
+
+import { APP_NAME, APP_SLOGAN } from "@nawadi/lib/constants";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { BASE_URL } from "~/constants";
+
+import { CommonProviders } from "./_components/providers";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: {
+    default: `${APP_NAME} | ${APP_SLOGAN}`,
+    template: `%s | ${APP_NAME}`,
+  },
+  applicationName: APP_NAME,
+  description: `Dé standaard voor veiligheid, kwaliteit en plezier op het water. Diplomalijn erkend door het Watersportverbond, met hoge kwaliteitseisen aan vaarlocaties.`,
+  metadataBase: BASE_URL,
+  icons: {
+    shortcut: "/favicon.ico",
+  },
+  openGraph: {
+    title: {
+      default: `${APP_NAME} | ${APP_SLOGAN}`,
+      template: `%s | ${APP_NAME}`,
+    },
+    description: `Dé standaard voor veiligheid, kwaliteit en plezier op het water. Diplomalijn erkend door het Watersportverbond, met hoge kwaliteitseisen aan vaarlocaties.`,
+    url: "/",
+    siteName: APP_NAME,
+    locale: "nl_NL",
+    type: "website",
+  },
+};
+
+export const runtime = "nodejs";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="nl"
+      className={clsx(inter.variable, "scroll-smooth text-gray-900")}
+    >
+      <body>
+        <CommonProviders>{children}</CommonProviders>
+        <SpeedInsights />
+      </body>
+    </html>
+  );
+}
