6f626c06b1cafbd668913deed78bc955616b646e
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-04-12 23:07:43 +0200

    fixing up repo setup (not done yet!)

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,3 +2,4 @@ export * as Actor from './actor.js'
 export * as Curriculum from './curriculum/index.js'
 export * as Location from './location/index.js'
 export * as Program from './program/index.js'
+export * from "./root.js"
--- packages/core/src/openapi/generator/specs.ts
+++ packages/core/src/openapi/generator/specs.ts
@@ -1,23 +1,11 @@
-import { createDocument } from 'zod-openapi'
-import { openApiObject } from '../index.js'
-
 import { writeFileSync } from 'fs'
 import yaml from 'js-yaml'
 import path from 'path'
-import { fileURLToPath } from 'url'
-
-function getProjectRoot() {
-  const __filename = fileURLToPath(import.meta.url)
-
-  const dirname = path.dirname(__filename)
-
-  return path.resolve(dirname, '../../../../..')
-}
+import { createDocument } from 'zod-openapi'
+import { projectRoot } from '../../root.js'
+import { openApiObject } from '../index.js'
 
 const spec = createDocument(openApiObject)
 const content = yaml.dump(spec, { indent: 2 })
 
-writeFileSync(
-  path.join(getProjectRoot(), 'specifications/api-gen.yaml'),
-  content,
-)
+writeFileSync(path.join(projectRoot, 'specifications/api-gen.yaml'), content)
