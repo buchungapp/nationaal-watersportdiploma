6f1e53b190c93376eb9a7dfaaebb9800eab014ae
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-04-14 11:58:40 +0200

    use not zod

--- apps/api-server/src/operation-handlers/programs/get-programs.ts
+++ apps/api-server/src/operation-handlers/programs/get-programs.ts
@@ -1,18 +1,30 @@
 import * as api from '@nawadi/api'
-import { Program } from '@nawadi/core'
+import { schema } from '@nawadi/db'
 import * as application from '../../application/index.js'
 
-export function getPrograms(
-  context: application.Context,
-): api.GetProgramsOperationHandler<application.Authentication> {
+export function getPrograms({
+  database,
+}: application.Context): api.GetProgramsOperationHandler<application.Authentication> {
   return async (incomingRequest, authentication) => {
-    const programs = await Program.list()
+    const programsRows = await database
+      .select({
+        id: schema.program.id,
+        title: schema.program.title,
+        handle: schema.program.handle,
+      })
+      .from(schema.program)
+
+    const programsEntity = programsRows.map((row) => ({
+      id: row.id,
+      title: row.title ?? '', // TODO this need support from the generator
+      handle: row.handle,
+    }))
 
     return {
       status: 200,
       parameters: {},
       contentType: 'application/json',
-      entity: () => [],
+      entity: () => programsEntity,
     }
   }
 }
