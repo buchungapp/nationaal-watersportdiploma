c4635cf74a943641d61d65937a7b158fa9fdcf06
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-04-14 12:04:07 +0200

    transaction for the operation

--- apps/api-server/src/operation-handlers/programs/get-programs.ts
+++ apps/api-server/src/operation-handlers/programs/get-programs.ts
@@ -5,27 +5,30 @@ import * as application from '../../application/index.js'
 export function getPrograms({
   database,
 }: application.Context): api.GetProgramsOperationHandler<application.Authentication> {
-  return async (incomingRequest, authentication) => {
-    const programsRows = await database
-      .select({
-        id: schema.program.id,
-        title: schema.program.title,
-        handle: schema.program.handle,
-      })
-      .from(schema.program)
+  return async (incomingRequest, authentication) =>
+    // we don't need a transaction here, but this illustrates one way of
+    // putting the entire operation in a transaction
+    database.transaction(async (transaction) => {
+      const programsRows = await transaction
+        .select({
+          id: schema.program.id,
+          title: schema.program.title,
+          handle: schema.program.handle,
+        })
+        .from(schema.program)
 
-    // programsRows could be used directly when the generator properly supports nulls.
-    const programsEntity = programsRows.map((row) => ({
-      id: row.id,
-      title: row.title ?? '', // TODO this need support from the generator
-      handle: row.handle,
-    }))
+      // programsRows could be used directly when the generator properly supports nulls.
+      const programsEntity = programsRows.map((row) => ({
+        id: row.id,
+        title: row.title ?? '', // TODO this need support from the generator
+        handle: row.handle,
+      }))
 
-    return {
-      status: 200,
-      parameters: {},
-      contentType: 'application/json',
-      entity: () => programsEntity,
-    }
-  }
+      return {
+        status: 200,
+        parameters: {},
+        contentType: 'application/json',
+        entity: () => programsEntity,
+      }
+    })
 }
