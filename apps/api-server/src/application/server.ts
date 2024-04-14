55e8e47bfaed35c1f39176f9faf0b1fb9657d7a6
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-04-14 14:35:36 +0200

    transactoin per request

--- apps/api-server/src/application/server.ts
+++ apps/api-server/src/application/server.ts
@@ -1,4 +1,5 @@
 import * as api from '@nawadi/api'
+import { withTransaction } from '@nawadi/core'
 import * as authenticationHandlers from '../authentication-handlers/index.js'
 import * as operationHandlers from '../operation-handlers/index.js'
 import { Authentication } from './authentication.js'
@@ -20,6 +21,12 @@ export function createApplicationServer() {
   // middleware!
 
   server.registerMiddleware(api.createErrorMiddleware())
+  server.registerMiddleware((request, next) =>
+    withTransaction(async () => {
+      const respone = await next(request)
+      return respone
+    }),
+  )
 
   return server
 }
