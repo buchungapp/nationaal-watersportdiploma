af6e2e62536bdbef4b0159e2e8fb4b3c2198c456
Maurits Misana <maurits@techmaus.nl>
2024-04-17 14:26:27 +0200

    update design

--- apps/web/src/app/(public)/_components/header/nav/media-kit.tsx
+++ apps/web/src/app/(public)/_components/header/nav/media-kit.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { Popover, Transition } from "@headlessui/react";
+import { HomeIcon, SparklesIcon } from "@heroicons/react/16/solid";
 import clsx from "clsx";
 import Link from "next/link";
-import { Fragment, useRef, useState } from "react";
+import React, { Fragment, useRef, useState } from "react";
 import Logo from "../../../../_components/brand/logo";
 import Wordmark from "../../../../_components/brand/wordmark";
 import { logo, wordmark } from "./media-kit-svgs";
@@ -33,39 +34,74 @@ export default function MediaKit() {
         leaveFrom="opacity-100 translate-y-0"
         leaveTo="opacity-0 translate-y-1"
       >
-        <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-xs rounded-3xl p-4 bg-white shadow-lg ring-1 ring-gray-900/5">
-          <CopyButton label="Kopieer Logo als SVG" value={logo} />
-          <CopyButton label="Kopieer Woordmerk als SVG" value={wordmark} />
-          <div className="group relative flex gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50">
-            <div className="flex-auto">
-              <a href={"/"} className="block font-semibold text-gray-900">
-                Naar de homepagina
-                <span className="absolute inset-0" />
-              </a>
-            </div>
-          </div>
+        <Popover.Panel className="absolute -left-4 top-full z-10 mt-3 w-screen max-w-xs rounded-xl px-2.5 py-4 bg-white shadow-lg ring-1 ring-gray-900/5">
+          <CopyButton
+            label={
+              <div className="flex items-center gap-x-2.5">
+                <span className="border-gray-600 border-2 block h-4 w-4 rounded-full" />
+                <p>Kopieer logo als SVG</p>
+              </div>
+            }
+            value={logo}
+          />
+          <CopyButton
+            label={
+              <div className="flex items-center gap-x-2.5">
+                <span className="font-serif text-gray-600 leading-5 w-4 text-center">
+                  N
+                </span>
+                <p>Kopieer woordmerk als SVG</p>
+              </div>
+            }
+            value={wordmark}
+          />
+          <Link
+            href="/merk"
+            className="relative flex items-center gap-x-2.5 rounded-lg px-4 py-2 text-sm leading-6 hover:bg-gray-50"
+          >
+            <SparklesIcon
+              className="h-4 w-4 text-gray-600"
+              aria-hidden={true}
+            />
+            <p className="block font-semibold text-gray-900">
+              Brand guidelines
+            </p>
+          </Link>
+          <Link
+            href="/"
+            className="relative flex items-center gap-x-2.5 rounded-lg px-4 py-2 text-sm leading-6 hover:bg-gray-50"
+          >
+            <HomeIcon className="h-4 w-4 text-gray-600" aria-hidden={true} />
+            <p className="block font-semibold text-gray-900">Homepagina</p>
+          </Link>
         </Popover.Panel>
       </Transition>
     </Popover>
   );
 }
 
-function CopyButton({ value, label }: { value: string; label: string }) {
+function CopyButton({
+  value,
+  label,
+}: {
+  value: string;
+  label: React.ReactNode;
+}) {
   const [showing, setShowing] = useState(false);
 
   return (
     <div
       className={clsx(
-        "group relative flex gap-x-6 rounded-lg p-4 text-sm leading-6 transition-colors",
+        "relative flex gap-x-2.5 rounded-lg px-4 py-2 text-sm leading-6 transition-colors",
         showing
           ? "bg-branding-dark text-white"
           : "hover:bg-gray-50 text-gray-900",
       )}
     >
       <button
-        onClick={() => {
+        onClick={async () => {
           if (typeof navigator.clipboard === "undefined") return;
-          navigator.clipboard.writeText(value);
+          await navigator.clipboard.writeText(value);
 
           setShowing(true);
           setTimeout(() => setShowing(false), 400);
@@ -73,7 +109,6 @@ function CopyButton({ value, label }: { value: string; label: string }) {
         className="font-semibold text-left w-full"
       >
         {label}
-        <span className="absolute inset-0" />
       </button>
     </div>
   );
