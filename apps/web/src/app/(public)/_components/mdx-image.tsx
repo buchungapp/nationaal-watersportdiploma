307ba3fd3000e63afd329b835b0b7c9930b3c00c
Maurits <maurits@techmaus.nl>
2024-04-30 22:58:33 +0200

    Chore/newsarticle diplomalijn (#136)

--- apps/web/src/app/(public)/_components/mdx-image.tsx
+++ apps/web/src/app/(public)/_components/mdx-image.tsx
@@ -1,8 +1,11 @@
 // https://github.com/hashicorp/next-mdx-remote/issues/405#issuecomment-1927215958
 
+import clsx from "clsx";
 import type { ImageProps } from "next/image";
 import Image from "next/image";
 
-export default function MdxImage(props: ImageProps) {
-  return <Image {...props} alt={props.alt} />;
+export default function MdxImage({ className, ...props }: ImageProps) {
+  return (
+    <Image {...props} alt={props.alt} className={clsx(className, "shadow")} />
+  );
 }
