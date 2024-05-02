edf4e84d373bdd029823742bd70d78ee0da67ee3
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-05-02 18:34:31 +0200

    cleanup

--- apps/api-server/src/authentication-handlers/api-key.test.ts
+++ apps/api-server/src/authentication-handlers/api-key.test.ts
@@ -6,23 +6,10 @@ import { withTestEnvironment } from '../testing/index.js'
 
 test('api-key authentication', () =>
   withTestEnvironment({ isolation: 'supabase' }, async ({ baseUrl }) => {
-    const supabaseClient = core.useSupabaseClient()
-
-    // const createUserResult = await supabaseClient.auth.admin.createUser({
-    //   email: 'test@test.test',
-    //   email_confirm: true,
-    //   password: 'supersecret',
-    // })
-    // assert.ifError(createUserResult.error)
-
-    // const authUserItem = createUserResult.data.user
-    // assert(authUserItem != null)
-
     const userItem = await core.User.getOrCreateFromEmail({
       email: 'test@test.test',
       displayName: 'test harry',
     })
-    // assert.equal(userItem.id, authUserItem.id)
 
     const apiKeyItem = await core.ApiKey.createForUser({
       name: 'test api-key',
