164a8ce51d7e99bcef65dcaea22c3fdbb4b8351c
Maurits Misana <maurits@techmaus.nl>
2024-05-04 10:53:02 +0200

    re-use confetti client

--- apps/web/src/app/(dashboard)/diploma/[id]/_components/confetti.tsx
+++ apps/web/src/app/(dashboard)/diploma/[id]/_components/confetti.tsx
@@ -1,31 +1,41 @@
 "use client";
 import JSConfetti from "js-confetti";
 import { useSearchParams } from "next/navigation";
-import { useEffect } from "react";
-
-function showConfetti() {
-  const jsConfetti = new JSConfetti();
-  void jsConfetti.addConfetti({
-    confettiColors: ["#ff8000", "#007FFF", "#0047ab"],
-    confettiRadius: 6,
-    confettiNumber: 500,
-  });
-}
+import { useCallback, useEffect, useMemo } from "react";
 
 export function Confetti() {
   const searchParams = useSearchParams();
+
+  const confettiClient = useMemo(() => {
+    if (typeof window === "undefined") {
+      return null;
+    }
+
+    return new JSConfetti();
+  }, []);
+
+  const showConfetti = useCallback(() => {
+    if (!confettiClient) {
+      console.info("Confetti client not initialized");
+      return;
+    }
+
+    void confettiClient.addConfetti({
+      confettiColors: ["#ff8000", "#007FFF", "#0047ab"],
+      confettiRadius: 6,
+      confettiNumber: 500,
+    });
+  }, [confettiClient]);
 
   useEffect(() => {
     if (searchParams.has("redirected")) {
       showConfetti();
     }
-  }, []);
+  }, [searchParams, showConfetti]);
 
   return (
     <button
-      onClick={() => {
-        showConfetti();
-      }}
+      onClick={showConfetti}
       className="mt-6 mx-auto px-4 py-2.5 bg-branding-orange text-white rounded-full font-medium"
     >
       Nog een keer vieren?! 🎉
