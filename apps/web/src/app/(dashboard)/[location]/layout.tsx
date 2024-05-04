db21270c15ff14195e8d396a2a7916007cd23bfb
U-THOMASB\thoma <t.ferb1@gmail.com>
2024-05-04 13:57:31 +0200

    chore: temporarly hide nav on mobile

--- apps/web/src/app/(dashboard)/[location]/layout.tsx
+++ apps/web/src/app/(dashboard)/[location]/layout.tsx
@@ -12,7 +12,7 @@ export default function Layout({
 }>) {
   return (
     <>
-      <div className="fixed inset-y-0 z-50 flex w-72 flex-col">
+      <div className="fixed inset-y-0 z-50 hidden w-72 flex-col sm:flex">
         <div className="flex grow flex-col gap-y-5 overflow-y-auto px-6">
           <div className="flex h-16 shrink-0 items-center">
             <img
