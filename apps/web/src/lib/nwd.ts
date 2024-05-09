af5cd9b972f7871d92d9ba2db0b2bf97ef4b6eec
Maurits Misana <maurits@techmaus.nl>
2024-05-09 11:11:30 +0200

    abstract request builder

--- apps/web/src/lib/nwd.ts
+++ apps/web/src/lib/nwd.ts
@@ -16,6 +16,21 @@ import { notFound, redirect } from "next/navigation";
 import { cache } from "react";
 import "server-only";
 
+async function makeRequest<T>(cb: () => Promise<T>) {
+  try {
+    return withSupabaseClient(
+      {
+        url: process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        serviceRoleKey: process.env.SUPABASE_SERVICE_ROLE_KEY!,
+      },
+      () => withDatabase({ pgUri: process.env.PGURI!, serverless: true }, cb),
+    );
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
 export const getUser = cache(async () => {
   const cookieStore = cookies();
 
@@ -39,13 +54,15 @@ export const getUser = cache(async () => {
 
   const { user: authUser } = userResponse.data;
 
-  const userData = await User.fromId(authUser.id);
+  return makeRequest(async () => {
+    const userData = await User.fromId(authUser.id);
 
-  if (!userData) {
-    throw new Error("User not found");
-  }
+    if (!userData) {
+      throw new Error("User not found");
+    }
 
-  return userData;
+    return userData;
+  });
 });
 
 export const findCertificate = async ({
@@ -55,391 +72,235 @@ export const findCertificate = async ({
   handle: string;
   issuedAt: string;
 }) => {
-  return withSupabaseClient(
-    {
-      url: process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      serviceRoleKey: process.env.SUPABASE_SERVICE_ROLE_KEY!,
-    },
-    () =>
-      withDatabase(
-        { pgUri: process.env.PGURI!, serverless: true },
-        async () => {
-          const certificate = await Certificate.find({
-            handle,
-            issuedAt,
-          });
+  return makeRequest(async () => {
+    const certificate = await Certificate.find({
+      handle,
+      issuedAt,
+    });
 
-          if (!certificate) {
-            notFound();
-          }
+    if (!certificate) {
+      notFound();
+    }
 
-          return certificate;
-        },
-      ),
-  );
+    return certificate;
+  });
 };
 
 export const listCertificatesByLocationId = cache(
   async (locationId: string) => {
-    return withSupabaseClient(
-      {
-        url: process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        serviceRoleKey: process.env.SUPABASE_SERVICE_ROLE_KEY!,
-      },
-      () =>
-        withDatabase(
-          { pgUri: process.env.PGURI!, serverless: true },
-          async () => {
-            await getUser();
+    return makeRequest(async () => {
+      await getUser();
 
-            const certificates = await Certificate.list({
-              filter: { locationId },
-            });
+      const certificates = await Certificate.list({
+        filter: { locationId },
+      });
 
-            return certificates;
-          },
-        ),
-    );
+      return certificates;
+    });
   },
 );
 
 export const listCertificatesByNumber = cache(async (numbers: string[]) => {
-  return withSupabaseClient(
-    {
-      url: process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      serviceRoleKey: process.env.SUPABASE_SERVICE_ROLE_KEY!,
-    },
-    () =>
-      withDatabase(
-        { pgUri: process.env.PGURI!, serverless: true },
-        async () => {
-          await getUser();
+  return makeRequest(async () => {
+    await getUser();
 
-          const certificates = await Certificate.list({
-            filter: { number: numbers },
-          });
+    const certificates = await Certificate.list({
+      filter: { number: numbers },
+    });
 
-          return certificates;
-        },
-      ),
-  );
+    return certificates;
+  });
 });
 
 export const retrieveCertificateById = cache(async (id: string) => {
-  return withSupabaseClient(
-    {
-      url: process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      serviceRoleKey: process.env.SUPABASE_SERVICE_ROLE_KEY!,
-    },
-    () =>
-      withDatabase(
-        { pgUri: process.env.PGURI!, serverless: true },
-        async () => {
-          const certificate = await Certificate.byId(id);
+  return makeRequest(async () => {
+    const certificate = await Certificate.byId(id);
 
-          if (!certificate) {
-            notFound();
-          }
+    if (!certificate) {
+      notFound();
+    }
 
-          return certificate;
-        },
-      ),
-  );
+    return certificate;
+  });
 });
 
 export const listDisciplines = cache(async () => {
-  return withSupabaseClient(
-    {
-      url: process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      serviceRoleKey: process.env.SUPABASE_SERVICE_ROLE_KEY!,
-    },
-    () =>
-      withDatabase(
-        { pgUri: process.env.PGURI!, serverless: true },
-        async () => {
-          const disciplines = await Program.Discipline.list();
+  return makeRequest(async () => {
+    const disciplines = await Program.Discipline.list();
 
-          return disciplines;
-        },
-      ),
-  );
+    return disciplines;
+  });
 });
 
 export const listCountries = cache(async () => {
-  return withSupabaseClient(
-    {
-      url: process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      serviceRoleKey: process.env.SUPABASE_SERVICE_ROLE_KEY!,
-    },
-    () =>
-      withDatabase(
-        { pgUri: process.env.PGURI!, serverless: true },
-        async () => {
-          const disciplines = await Platform.Country.list();
+  return makeRequest(async () => {
+    const disciplines = await Platform.Country.list();
 
-          return disciplines;
-        },
-      ),
-  );
+    return disciplines;
+  });
 });
 
 export const retrieveDisciplineByHandle = cache(async (handle: string) => {
-  return withSupabaseClient(
-    {
-      url: process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      serviceRoleKey: process.env.SUPABASE_SERVICE_ROLE_KEY!,
-    },
-    () =>
-      withDatabase(
-        { pgUri: process.env.PGURI!, serverless: true },
-        async () => {
-          const disciplines = await Program.Discipline.fromHandle(handle);
+  return makeRequest(async () => {
+    const disciplines = await Program.Discipline.fromHandle(handle);
 
-          return disciplines;
-        },
-      ),
-  );
+    return disciplines;
+  });
 });
 
 export const listPrograms = cache(async () => {
-  return withSupabaseClient(
-    {
-      url: process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      serviceRoleKey: process.env.SUPABASE_SERVICE_ROLE_KEY!,
-    },
-    () =>
-      withDatabase(
-        { pgUri: process.env.PGURI!, serverless: true },
-        async () => {
-          const disciplines = await Program.list();
+  return makeRequest(async () => {
+    const disciplines = await Program.list();
 
-          return disciplines;
-        },
-      ),
-  );
+    return disciplines;
+  });
 });
 
 export const listCurriculaByDiscipline = cache(async (disciplineId: string) => {
-  return withSupabaseClient(
-    {
-      url: process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      serviceRoleKey: process.env.SUPABASE_SERVICE_ROLE_KEY!,
-    },
-    () =>
-      withDatabase(
-        { pgUri: process.env.PGURI!, serverless: true },
-        async () => {
-          const disciplines = await Curriculum.list({
-            filter: { onlyCurrentActive: true, disciplineId },
-          });
+  return makeRequest(async () => {
+    const disciplines = await Curriculum.list({
+      filter: { onlyCurrentActive: true, disciplineId },
+    });
 
-          return disciplines;
-        },
-      ),
-  );
+    return disciplines;
+  });
 });
 
 export const listCurriculaByProgram = cache(async (programId: string) => {
-  return withSupabaseClient(
-    {
-      url: process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      serviceRoleKey: process.env.SUPABASE_SERVICE_ROLE_KEY!,
-    },
-    () =>
-      withDatabase(
-        { pgUri: process.env.PGURI!, serverless: true },
-        async () => {
-          const disciplines = await Curriculum.list({
-            filter: { onlyCurrentActive: true, programId },
-          });
+  return makeRequest(async () => {
+    const disciplines = await Curriculum.list({
+      filter: { onlyCurrentActive: true, programId },
+    });
 
-          return disciplines;
-        },
-      ),
-  );
+    return disciplines;
+  });
 });
 
 export const listGearTypesByCurriculum = cache(async (curriculumId: string) => {
-  return withSupabaseClient(
-    {
-      url: process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      serviceRoleKey: process.env.SUPABASE_SERVICE_ROLE_KEY!,
-    },
-    () =>
-      withDatabase(
-        { pgUri: process.env.PGURI!, serverless: true },
-        async () => {
-          const gearTypes = await Curriculum.GearType.list({
-            filter: {
-              curriculumId: curriculumId,
-            },
-          });
+  return makeRequest(async () => {
+    const gearTypes = await Curriculum.GearType.list({
+      filter: {
+        curriculumId: curriculumId,
+      },
+    });
 
-          return gearTypes;
-        },
-      ),
-  );
+    return gearTypes;
+  });
 });
 
 export const retrieveLocationByHandle = cache(async (handle: string) => {
-  return withSupabaseClient(
-    {
-      url: process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      serviceRoleKey: process.env.SUPABASE_SERVICE_ROLE_KEY!,
-    },
-    () =>
-      withDatabase(
-        { pgUri: process.env.PGURI!, serverless: true },
-        async () => {
-          await getUser();
+  return makeRequest(async () => {
+    await getUser();
 
-          const location = await Location.fromHandle(handle);
+    const location = await Location.fromHandle(handle);
 
-          return location;
-        },
-      ),
-  );
+    return location;
+  });
 });
 
 export const listPersonsForLocation = cache(async (locationId: string) => {
-  return withSupabaseClient(
-    {
-      url: process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      serviceRoleKey: process.env.SUPABASE_SERVICE_ROLE_KEY!,
-    },
-    () =>
-      withDatabase(
-        { pgUri: process.env.PGURI!, serverless: true },
-        async () => {
-          await getUser();
+  return makeRequest(async () => {
+    await getUser();
 
-          const persons = await User.Person.list({ filter: { locationId } });
+    const persons = await User.Person.list({ filter: { locationId } });
 
-          return persons;
-        },
-      ),
-  );
+    return persons;
+  });
 });
 
-export const createPersonForLocation = cache(
-  async (
-    locationId: string,
-    personInput: {
-      email: string;
-      firstName: string;
-      lastNamePrefix: string | null;
-      lastName: string;
-      dateOfBirth: Date;
-      birthCity: string;
-      birthCountry: string;
-    },
-  ) => {
-    return withSupabaseClient(
-      {
-        url: process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        serviceRoleKey: process.env.SUPABASE_SERVICE_ROLE_KEY!,
-      },
-      () =>
-        withDatabase(
-          { pgUri: process.env.PGURI!, serverless: true },
-          async () => {
-            await getUser();
-
-            let user;
-
-            if (personInput.email) {
-              user = await User.getOrCreateFromEmail({
-                email: personInput.email,
-                displayName: personInput.firstName,
-              });
-            }
-
-            const person = await User.Person.getOrCreate({
-              userId: user?.id,
-              firstName: personInput.firstName,
-              lastName: personInput.lastName,
-              lastNamePrefix: personInput.lastNamePrefix,
-              dateOfBirth: personInput.dateOfBirth.toISOString(),
-              birthCity: personInput.birthCity,
-              birthCountry: personInput.birthCountry,
-            });
-
-            await User.Person.createLocationLink({
-              personId: person.id,
-              locationId: locationId,
-            });
-
-            await User.Actor.upsert({
-              locationId: locationId,
-              type: "student",
-              personId: person.id,
-            });
-
-            return person;
-          },
-        ),
-    );
+export const createPersonForLocation = async (
+  locationId: string,
+  personInput: {
+    email: string;
+    firstName: string;
+    lastNamePrefix: string | null;
+    lastName: string;
+    dateOfBirth: Date;
+    birthCity: string;
+    birthCountry: string;
   },
-);
+) => {
+  return makeRequest(async () => {
+    await getUser();
 
-export const createCompletedCertificate = cache(
-  async (
-    locationId: string,
-    personId: string,
-    {
-      curriculumId,
-      gearTypeId,
-      competencies,
-    }: {
-      curriculumId: string;
-      gearTypeId: string;
-      competencies: string[];
-    },
-  ) => {
-    return withSupabaseClient(
-      {
-        url: process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        serviceRoleKey: process.env.SUPABASE_SERVICE_ROLE_KEY!,
-      },
-      () =>
-        withDatabase(
-          { pgUri: process.env.PGURI!, serverless: true },
-          async () => {
-            return withTransaction(async () => {
-              await getUser();
+    let user;
 
-              // Start student curriculum
-              const { id: studentCurriculumId } =
-                await Student.Program.startProgram({
-                  curriculumId,
-                  personId,
-                  gearTypeId,
-                });
+    if (personInput.email) {
+      user = await User.getOrCreateFromEmail({
+        email: personInput.email,
+        displayName: personInput.firstName,
+      });
+    }
 
-              // Start certificate
-              const { id: certificateId } =
-                await Student.Certificate.startCertificate({
-                  locationId,
-                  studentCurriculumId,
-                });
+    const person = await User.Person.getOrCreate({
+      userId: user?.id,
+      firstName: personInput.firstName,
+      lastName: personInput.lastName,
+      lastNamePrefix: personInput.lastNamePrefix,
+      dateOfBirth: personInput.dateOfBirth.toISOString(),
+      birthCity: personInput.birthCity,
+      birthCountry: personInput.birthCountry,
+    });
 
-              // Add completed competencies
-              await Student.Certificate.completeCompetency({
-                certificateId,
-                studentCurriculumId,
-                competencyId: competencies.flat(),
-              });
+    await User.Person.createLocationLink({
+      personId: person.id,
+      locationId: locationId,
+    });
 
-              // Complete certificate
-              await Student.Certificate.completeCertificate({
-                certificateId,
-                visibleFrom: new Date().toISOString(),
-              });
+    await User.Actor.upsert({
+      locationId: locationId,
+      type: "student",
+      personId: person.id,
+    });
 
-              return { id: certificateId };
-            });
-          },
-        ),
-    );
+    return person;
+  });
+};
+
+export const createCompletedCertificate = async (
+  locationId: string,
+  personId: string,
+  {
+    curriculumId,
+    gearTypeId,
+    competencies,
+  }: {
+    curriculumId: string;
+    gearTypeId: string;
+    competencies: string[];
   },
-);
+) => {
+  return makeRequest(async () => {
+    return withTransaction(async () => {
+      await getUser();
+
+      // Start student curriculum
+      const { id: studentCurriculumId } = await Student.Program.startProgram({
+        curriculumId,
+        personId,
+        gearTypeId,
+      });
+
+      // Start certificate
+      const { id: certificateId } = await Student.Certificate.startCertificate({
+        locationId,
+        studentCurriculumId,
+      });
+
+      // Add completed competencies
+      await Student.Certificate.completeCompetency({
+        certificateId,
+        studentCurriculumId,
+        competencyId: competencies.flat(),
+      });
+
+      // Complete certificate
+      await Student.Certificate.completeCertificate({
+        certificateId,
+        visibleFrom: new Date().toISOString(),
+      });
+
+      return { id: certificateId };
+    });
+  });
+};
