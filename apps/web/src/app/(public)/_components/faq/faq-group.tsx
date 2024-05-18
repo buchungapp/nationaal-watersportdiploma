3665c9c06c37cb7bcb434bced3bd748f28eb5d86
Maurits Misana <maurits@techmaus.nl>
2024-05-18 19:26:53 +0200

    add ld+json to faq group

--- apps/web/src/app/(public)/_components/faq/faq-group.tsx
+++ apps/web/src/app/(public)/_components/faq/faq-group.tsx
@@ -3,17 +3,38 @@ import Faq from "./faq";
 
 export default function FaqGroup({ faqs }: { faqs: FaqType[] }) {
   return (
-    <dl className="divide-y divide-gray-900/10 rounded-lg border border-gray-900/10">
-      {faqs.length < 1 ? (
-        <div className="p-4 text-center text-gray-900/50">
-          Er zijn geen vragen gevonden.
-        </div>
-      ) : null}
-      {faqs.map(({ answer, question }) => (
-        <Faq key={question} question={question}>
-          <div dangerouslySetInnerHTML={{ __html: answer }} />
-        </Faq>
-      ))}
-    </dl>
+    <>
+      <script
+        type="application/ld+json"
+        suppressHydrationWarning
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "FAQPage",
+            mainEntity: faqs.map(({ answer, question }) => ({
+              "@type": "Question",
+              name: question,
+              acceptedAnswer: {
+                "@type": "Answer",
+                text: answer,
+              },
+            })),
+          }),
+        }}
+      />
+
+      <dl className="divide-y divide-gray-900/10 rounded-lg border border-gray-900/10">
+        {faqs.length < 1 ? (
+          <div className="p-4 text-center text-gray-900/50">
+            Er zijn geen vragen gevonden.
+          </div>
+        ) : null}
+        {faqs.map(({ answer, question }) => (
+          <Faq key={question} question={question}>
+            <div dangerouslySetInnerHTML={{ __html: answer }} />
+          </Faq>
+        ))}
+      </dl>
+    </>
   );
 }
