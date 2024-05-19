e7b0f4411372f973af61f1ed84e39a396ff2bd2e
Maurits Misana <maurits@techmaus.nl>
2024-05-19 14:41:19 +0200

    add cta image placeholder

--- apps/web/src/app/(public)/_components/cta.tsx
+++ apps/web/src/app/(public)/_components/cta.tsx
@@ -6,7 +6,12 @@ export function FindNWDCourse() {
   return (
     <div className="relative bg-branding-dark rounded-3xl overflow-hidden">
       <div className="relative h-80 overflow-hidden md:absolute md:left-0 md:h-full md:w-1/3 lg:w-1/2">
-        <Image className="h-full w-full object-cover" src={Bahia} alt="" />
+        <Image
+          placeholder="blur"
+          className="h-full w-full object-cover"
+          src={Bahia}
+          alt=""
+        />
       </div>
       <div className="relative mx-auto max-w-7xl py-24 sm:py-32 lg:px-8 lg:py-40">
         <div className="pl-6 pr-6 md:ml-auto md:w-2/3 md:pl-16 lg:w-1/2 lg:pl-24 lg:pr-0 xl:pl-32">
