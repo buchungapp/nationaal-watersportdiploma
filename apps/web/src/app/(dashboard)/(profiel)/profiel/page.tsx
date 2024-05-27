a1e5ee5b76dd8a11defee1befbbe2f854d1d0b99
Maurits Misana <maurits@techmaus.nl>
2024-05-27 18:48:02 +0200

    start working on profile section

--- apps/web/src/app/(dashboard)/(profiel)/profiel/page.tsx
+++ apps/web/src/app/(dashboard)/(profiel)/profiel/page.tsx
@@ -1,36 +1,37 @@
-import { logout } from "~/app/_actions/auth";
 import { listLocationsForPerson } from "~/lib/nwd";
-import { Button } from "../_components/button";
-import { Link } from "../_components/link";
+import { Button } from "../../_components/button";
+import { Link } from "../../_components/link";
+import { Text } from "../../_components/text";
 
 export default async function Page() {
   const locations = await listLocationsForPerson();
 
   return (
-    <div className="p-4 prose max-w-prose mx-auto">
-      <p>
+    <div className="p-4 max-w-prose mx-auto">
+      <Text>
         He pionier, je hebt work-in-progress gevonden. Op dit moment werken we
         namelijk hard aan het implementeren van de nieuwe applicatie. Zodra het
         zover is, zullen we je hierover informeren.
-      </p>
+      </Text>
 
-      <h2>Wat nu?</h2>
-      <ul>
-        <li>
-          <Button href="/">Terug naar de homepage</Button>
-        </li>
-        <li>
-          <Button href="https://www.nationaalwatersportdiploma.dev">
-            Volg de ontwikkelingen via GitHub
-          </Button>
-        </li>
-        <li>
-          <Button onClick={logout}>Volg de ontwikkelingen via GitHub</Button>
-        </li>
-      </ul>
+      <div className="my-6">
+        <h2>Wat nu?</h2>
+        <ul>
+          <li>
+            <Button href="/" plain>
+              Terug naar de homepage
+            </Button>
+          </li>
+          <li>
+            <Button href="https://www.nationaalwatersportdiploma.dev" plain>
+              Volg de ontwikkelingen via GitHub
+            </Button>
+          </li>
+        </ul>
+      </div>
 
       {locations.length > 0 ? (
-        <>
+        <div className="my-6">
           <h2>Beheer locatie</h2>
           <ul>
             {locations.map((location) => (
@@ -41,7 +42,7 @@ export default async function Page() {
               </li>
             ))}
           </ul>
-        </>
+        </div>
       ) : null}
     </div>
   );
