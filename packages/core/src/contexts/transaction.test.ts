1354eb216be44cb8d07948b5190b9c3675b48723
Maurits Misana <maurits@techmaus.nl>
2024-06-02 11:24:31 +0200

    add test case for double transaction

--- packages/core/src/contexts/transaction.test.ts
+++ packages/core/src/contexts/transaction.test.ts
@@ -2,7 +2,7 @@ import assert from 'assert'
 import { sql } from 'drizzle-orm'
 import test from 'node:test'
 import { useQuery } from './query.js'
-import { withTestTransaction } from './transaction.js'
+import { withTestTransaction, withTransaction } from './transaction.js'
 
 test('with-test-transaction', () =>
   withTestTransaction(async () => {
@@ -15,4 +15,28 @@ test('with-test-transaction', () =>
         one: 1,
       },
     ])
+  }))
+
+test('withTransaction should not retry job on error', async () =>
+  withTestTransaction(async () => {
+    let transactionCount = 0
+
+    async function failingJob() {
+      transactionCount += 1
+      // Simulate a job that throws an error
+      throw new Error('Test error')
+    }
+
+    try {
+      await withTransaction(failingJob)
+    } catch (error) {
+      if (error instanceof Error) {
+        assert.strictEqual(error.message, 'Test error')
+      } else {
+        throw error // Re-throw if it's not an Error instance
+      }
+    }
+
+    // Ensure the job was only executed once
+    assert.strictEqual(transactionCount, 1, 'Job should not be retried')
   }))
