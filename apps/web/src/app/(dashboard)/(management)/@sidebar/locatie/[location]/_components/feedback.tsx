3eca0e1861819810e078a58e49800494b149eb12
Anton Kouliavsev <seoanton@gmail.com>
2024-06-21 16:45:34 +0200

    Add Tabs

--- apps/web/src/app/(dashboard)/(management)/@sidebar/locatie/[location]/_components/feedback.tsx
+++ apps/web/src/app/(dashboard)/(management)/@sidebar/locatie/[location]/_components/feedback.tsx
@@ -21,33 +21,39 @@ import {
   DialogTitle,
 } from "~/app/(dashboard)/_components/dialog";
 
+import { toast } from "sonner";
 import {
   Description,
   Field,
-  FieldGroup,
   Fieldset,
   Label,
 } from "~/app/(dashboard)/_components/fieldset";
 
+import { Tab, TabGroup, TabList } from "@tremor/react";
 import { Button } from "~/app/(dashboard)/_components/button";
-import {
-  Radio,
-  RadioField,
-  RadioGroup,
-} from "~/app/(dashboard)/_components/radio";
 import { Textarea } from "~/app/(dashboard)/_components/textarea";
 
 import { useFormState as useActionState, useFormStatus } from "react-dom";
 
+const mockSubmitFeedback = (prevState: unknown, formData: FormData) => {
+  console.log("✅ prevState", prevState);
+  console.log("✅ formData", formData);
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({ message: "Success" });
+    }, 1500);
+  });
+};
+
 export function Feedback() {
-  // TODO: Rewrite this part
   const submit = async (prevState: unknown, formData: FormData) => {
-    // const result = await createPerson(locationId, prevState, formData);
-    // if (result.message === "Success") {
-    //   setIsOpen(false);
-    //   toast.success("Persoon is toegevoegd.");
-    // }
-    // return result;
+    const result = await mockSubmitFeedback(prevState, formData);
+    if (result?.message === "Success") {
+      setIsOpen(false);
+      toast.success("Dank! Jouw feedback is ontvangen! 🎉");
+    }
+    return result;
   };
 
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -62,45 +68,38 @@ export function Feedback() {
 
       <Dialog open={isOpen} onClose={setIsOpen}>
         <DialogTitle>Help mij</DialogTitle>
-        <DialogDescription>Er zijn geen stomme vragen.</DialogDescription>
+        <DialogDescription>
+          Kies het type vraag je wilt stellen.
+        </DialogDescription>
         <form action={formAction}>
           <DialogBody>
             <Fieldset>
-              <FieldGroup>
-                {/* TODO: first iteration fields (use tabs) */}
-                <RadioGroup name="resale" defaultValue="permit">
-                  <RadioField>
-                    <Radio value="bug" />
-                    <Label>Bug</Label>
-                  </RadioField>
-                  <RadioField>
-                    <Radio value="feedback" />
-                    <Label>Feedback</Label>
-                  </RadioField>
-                  <RadioField>
-                    <Radio value="question" />
-                    <Label>Question</Label>
-                  </RadioField>
-                </RadioGroup>
+              <TabGroup className="mb-6">
+                <TabList variant="solid" defaultValue="bug">
+                  <Tab value="bug">Bug</Tab>
+                  <Tab value="feedback">Feedback</Tab>
+                  <Tab value="question">Vraag</Tab>
+                </TabList>
+              </TabGroup>
 
-                <Field>
-                  <Label>Opmerking</Label>
-                  <Textarea name="comment" required />
-                </Field>
+              <Field>
+                <Label>Opmerking</Label>
+                <Textarea name="comment" required />
+              </Field>
 
-                <CheckboxGroup>
-                  <CheckboxField>
-                    <Checkbox name="urgent" />
-                    <Label>Dit is dringend</Label>
-                    <Description>
-                      Vink aan als dit als de bug voorkomt dat je Plain kunt
-                      gebruiken.
-                    </Description>
-                  </CheckboxField>
-                </CheckboxGroup>
-              </FieldGroup>
+              <CheckboxGroup>
+                <CheckboxField>
+                  <Checkbox name="urgent" />
+                  <Label>Dit is dringend</Label>
+                  <Description>
+                    Vink aan als dit als de bug voorkomt dat je Plain kunt
+                    gebruiken.
+                  </Description>
+                </CheckboxField>
+              </CheckboxGroup>
             </Fieldset>
           </DialogBody>
+
           <DialogActions>
             <Button plain onClick={() => setIsOpen(false)}>
               Sluiten
