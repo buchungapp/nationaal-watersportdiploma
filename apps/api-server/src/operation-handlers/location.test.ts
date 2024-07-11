b71250ad84dff559ace2ca5189d33773c3259dc9
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-07-11 14:23:24 +0200

    cleanup

--- apps/api-server/src/operation-handlers/location.test.ts
+++ apps/api-server/src/operation-handlers/location.test.ts
@@ -4,36 +4,33 @@ import test from 'node:test'
 import { withTestEnvironment } from '../testing/index.js'
 
 test('location crud', () =>
-  withTestEnvironment(
-    { isolation: 'transaction' },
-    async ({ server, baseUrl }) => {
-      server.registerApiKeyAuthentication(async (token) => {
-        switch (token) {
-          case 'supersecret':
-            return {
-              apiKey: '00000000-0000-0000-0000-000000000000',
-              user: '00000000-0000-0000-0000-000000000000',
-            }
+  withTestEnvironment({ isolation: 'transaction' }, async ({ server }) => {
+    server.registerApiKeyAuthentication(async (token) => {
+      switch (token) {
+        case 'supersecret':
+          return {
+            apiKey: '00000000-0000-0000-0000-000000000000',
+            user: '00000000-0000-0000-0000-000000000000',
+          }
 
-          default:
-            return
-        }
+        default:
+          return
+      }
+    })
+    api.facade.defaultCredentials.apiKey = 'supersecret'
+
+    let id
+    {
+      const item = await api.facade.createLocation({
+        title: 'title-123',
+        handle: 'handle-123',
       })
-      api.facade.defaultCredentials.apiKey = 'supersecret'
+      id = item.id
+    }
 
-      let id
-      {
-        const item = await api.facade.createLocation({
-          title: 'title-123',
-          handle: 'handle-123',
-        })
-        id = item.id
-      }
-
-      {
-        const list = await api.facade.getLocations()
-        assert.equal(list.length, 1)
-        assert.equal(list[0]?.id, id)
-      }
-    },
-  ))
+    {
+      const list = await api.facade.getLocations()
+      assert.equal(list.length, 1)
+      assert.equal(list[0]?.id, id)
+    }
+  }))
