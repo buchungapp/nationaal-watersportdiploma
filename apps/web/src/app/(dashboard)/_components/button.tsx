7f1db2b53d7576112c5089d3045fca03b9a4f92b
Maurits <maurits@techmaus.nl>
2024-07-19 15:41:12 +0200

    add course title to searchable fields (#225)

--- apps/web/src/app/(dashboard)/_components/button.tsx
+++ apps/web/src/app/(dashboard)/_components/button.tsx
@@ -224,6 +224,7 @@ export const Button = React.forwardRef(function Button(
       {...props}
       className={clsx(classes, "cursor-default")}
       ref={ref}
+      data-disabled={props.disabled}
     >
       <TouchTarget>{children}</TouchTarget>
     </Headless.Button>
