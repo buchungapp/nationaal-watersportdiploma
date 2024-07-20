0696984072b68366886704317d0edba09c6f2975
Maurits Misana <maurits@techmaus.nl>
2024-07-20 18:29:33 +0200

    run formatter

--- apps/web/src/lib/dayjs.ts
+++ apps/web/src/lib/dayjs.ts
@@ -1,31 +1,31 @@
-import dayjs from 'dayjs'
-import 'dayjs/locale/nl'
+import dayjs from "dayjs";
+import "dayjs/locale/nl";
 
-import customParseFormat from 'dayjs/plugin/customParseFormat.js'
-import duration from 'dayjs/plugin/duration.js'
-import isBetween from 'dayjs/plugin/isBetween.js'
-import isToday from 'dayjs/plugin/isToday.js'
-import localizedFormat from 'dayjs/plugin/localizedFormat.js'
-import minmax from 'dayjs/plugin/minMax.js'
-import relativeTime from 'dayjs/plugin/relativeTime.js'
-import timeZone from 'dayjs/plugin/timezone.js'
-import toArray from 'dayjs/plugin/toArray.js'
-import utc from 'dayjs/plugin/utc.js'
+import customParseFormat from "dayjs/plugin/customParseFormat.js";
+import duration from "dayjs/plugin/duration.js";
+import isBetween from "dayjs/plugin/isBetween.js";
+import isToday from "dayjs/plugin/isToday.js";
+import localizedFormat from "dayjs/plugin/localizedFormat.js";
+import minmax from "dayjs/plugin/minMax.js";
+import relativeTime from "dayjs/plugin/relativeTime.js";
+import timeZone from "dayjs/plugin/timezone.js";
+import toArray from "dayjs/plugin/toArray.js";
+import utc from "dayjs/plugin/utc.js";
 
-dayjs.extend(customParseFormat)
-dayjs.extend(isBetween)
-dayjs.extend(isToday)
-dayjs.extend(localizedFormat)
-dayjs.extend(relativeTime)
-dayjs.extend(timeZone)
-dayjs.extend(toArray)
-dayjs.extend(utc)
-dayjs.extend(minmax)
-dayjs.extend(duration)
+dayjs.extend(customParseFormat);
+dayjs.extend(isBetween);
+dayjs.extend(isToday);
+dayjs.extend(localizedFormat);
+dayjs.extend(relativeTime);
+dayjs.extend(timeZone);
+dayjs.extend(toArray);
+dayjs.extend(utc);
+dayjs.extend(minmax);
+dayjs.extend(duration);
 
-dayjs.locale('nl')
+dayjs.locale("nl");
 
-export type Dayjs = dayjs.Dayjs
-export type { ConfigType } from 'dayjs'
+export type Dayjs = dayjs.Dayjs;
+export type { ConfigType } from "dayjs";
 
-export default dayjs
+export default dayjs;
