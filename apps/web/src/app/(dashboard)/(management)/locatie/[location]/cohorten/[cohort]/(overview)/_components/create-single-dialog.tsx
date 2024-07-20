653f9c67cda9070c751c0f8cb433ee9b9fa15713
Maurits Misana <maurits@techmaus.nl>
2024-07-20 23:25:33 +0200

    respect nullable date of birth for person

--- apps/web/src/app/(dashboard)/(management)/locatie/[location]/cohorten/[cohort]/(overview)/_components/create-single-dialog.tsx
+++ apps/web/src/app/(dashboard)/(management)/locatie/[location]/cohorten/[cohort]/(overview)/_components/create-single-dialog.tsx
@@ -301,7 +301,9 @@ function CreateDialog({ locationId, cohortId, isOpen, setIsOpen }: Props) {
                                 "ml-2 truncate text-gray-500 group-data-[active]/option:text-white",
                               )}
                             >
-                              {dayjs(person.dateOfBirth).format("DD-MM-YYYY")}
+                              {person.dateOfBirth
+                                ? dayjs(person.dateOfBirth).format("DD-MM-YYYY")
+                                : null}
                             </span>
                           </div>
                         </ComboboxLabel>
