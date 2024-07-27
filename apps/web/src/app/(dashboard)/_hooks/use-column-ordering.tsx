02533c069debfe150377f8d1e274e34d16b41a09
Maurits Misana <maurits@techmaus.nl>
2024-07-27 10:44:22 +0200

    use searchparams

--- apps/web/src/app/(dashboard)/_hooks/use-column-ordering.tsx
+++ apps/web/src/app/(dashboard)/_hooks/use-column-ordering.tsx
@@ -1,31 +1,54 @@
 import type { ColumnDef } from "@tanstack/react-table";
-import { useState } from "react";
+import {
+  parseAsArrayOf,
+  parseAsJson,
+  parseAsString,
+  useQueryState,
+} from "nuqs";
+import React from "react";
+
+type OrderableColumn<TData, TValue> = ColumnDef<TData, TValue> & {
+  id: string;
+};
 
 export function useColumnOrdering<TData, TValue>(
-  columns: ColumnDef<TData, TValue>[],
+  orderableColumns: OrderableColumn<TData, TValue>[],
 ) {
-  const [columnOrder, setColumnOrder] = useState<string[]>(() =>
-    columns.filter((column) => !!column.id).map((column) => column.id!),
+  const defaultOrder = React.useMemo(
+    () => orderableColumns.map((column) => column.id),
+    [],
+  );
+  const defaultVisibility = React.useMemo(
+    () =>
+      orderableColumns.reduce(
+        (
+          acc: Record<string, boolean>,
+          column: {
+            id?: string;
+            isDefaultVisible?: boolean;
+          },
+        ) => {
+          if (
+            column.id !== undefined &&
+            column.isDefaultVisible !== undefined
+          ) {
+            acc[column.id] = column.isDefaultVisible;
+          }
+          return acc;
+        },
+        {},
+      ),
+    [],
   );
 
-  const [columnVisibility, setColumnVisibility] = useState<
-    Record<string, boolean>
-  >(
-    columns.reduce(
-      (
-        acc: Record<string, boolean>,
-        column: {
-          id?: string;
-          isDefaultVisible?: boolean;
-        },
-      ) => {
-        if (column.id !== undefined && column.isDefaultVisible !== undefined) {
-          acc[column.id] = column.isDefaultVisible;
-        }
-        return acc;
-      },
-      {},
-    ),
+  const [columnOrder, setColumnOrder] = useQueryState(
+    "volgorde",
+    parseAsArrayOf(parseAsString).withDefault(defaultOrder),
+  );
+
+  const [columnVisibility, setColumnVisibility] = useQueryState(
+    "toon",
+    parseAsJson().withDefault(defaultVisibility),
   );
 
   const options = {
