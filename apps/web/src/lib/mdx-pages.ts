82e6656551e4f8f78edd4ffed226c890920f3115
U-THOMASB\thoma <t.ferb1@gmail.com>
2024-07-29 14:39:15 +0200

    tmp: added logs to see what is happning in the vercel build

--- apps/web/src/lib/mdx-pages.ts
+++ apps/web/src/lib/mdx-pages.ts
@@ -15,6 +15,8 @@ export async function getAllDiplomalijnInstructeurPages() {
     cwd: path.join(workingPath, contentPath, "diplomalijn/instructeur"),
   });
 
+  console.log("instructeur: pageFilenames", pageFilenames);
+
   const pages = await Promise.all(
     pageFilenames.map(async (pageFilename) => {
       const { page } = (await import(
@@ -36,6 +38,8 @@ export async function getAllDiplomalijnInstructeurPages() {
     }),
   );
 
+  console.log("instructeur: pages", pages);
+
   return pages.sort((a, z) => {
     // Sort on order if available
     if (a.order && z.order) return a.order - z.order;
@@ -52,6 +56,8 @@ export async function getAllDiplomalijnConsumentenPages() {
   const pageFilenames = await glob("**/page.mdx", {
     cwd: path.join(workingPath, contentPath, "diplomalijn/consument"),
   });
+
+  console.log("consument: pageFilenames", pageFilenames);
 
   const pages = await Promise.all(
     pageFilenames.map(async (pageFilename) => {
@@ -73,6 +79,8 @@ export async function getAllDiplomalijnConsumentenPages() {
       };
     }),
   );
+
+  console.log("consument: pages", pages);
 
   return pages.sort((a, z) => {
     // Sort on order if available
