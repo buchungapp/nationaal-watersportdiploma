a8529f14d38c9aa6332bc910ed456705641fa4e9
Thomas (DJ1TJOO) <44841260+DJ1TJOO@users.noreply.github.com>
2024-07-30 09:20:31 +0200

    Feat/table use sorting (#245)
    Adds `useSorting` hook for tables
    
    - added: useSorting
    - refactor: useColumnOrdering to be similair
    - fix: z-index on column order items
    
    ---------
    
    Co-authored-by: Maurits Misana <maurits@techmaus.nl>

--- apps/web/src/app/(dashboard)/_hooks/use-column-ordering.tsx
+++ apps/web/src/app/(dashboard)/_hooks/use-column-ordering.tsx
@@ -1,15 +1,16 @@
-import type { ColumnDef } from "@tanstack/react-table";
 import {
   createParser,
   parseAsArrayOf,
   parseAsString,
   useQueryState,
 } from "nuqs";
-import React from "react";
+import React, { useMemo } from "react";
+import type { Optional } from "~/types/optional";
 
-type OrderableColumn<TData, TValue> = ColumnDef<TData, TValue> & {
+interface OrderableColumn {
   id: string;
-};
+  isDefaultVisible?: boolean;
+}
 
 const parseAsColumnVisibility = (validColumnKeys: string[]) => {
   return createParser<Record<string, boolean>>({
@@ -31,9 +32,20 @@ const parseAsColumnVisibility = (validColumnKeys: string[]) => {
   });
 };
 
-export function useColumnOrdering<TData, TValue>(
-  orderableColumns: OrderableColumn<TData, TValue>[],
-) {
+export function getOrderableColumnIds({
+  columns,
+  excludeColumns,
+}: {
+  columns: Optional<OrderableColumn, "id">[];
+  excludeColumns?: string[];
+}) {
+  return columns.filter(
+    (c): c is typeof c & { id: string } =>
+      !!c.id && !excludeColumns?.includes(c.id),
+  );
+}
+
+export function useColumnOrdering(orderableColumns: OrderableColumn[]) {
   const defaultOrder = React.useMemo(
     () => orderableColumns.map((column) => column.id),
     [orderableColumns],
@@ -73,14 +85,22 @@ export function useColumnOrdering<TData, TValue>(
     parseAsColumnVisibility(defaultOrder).withDefault(defaultVisibility),
   );
 
-  const options = {
-    state: {
-      columnOrder,
-      columnVisibility,
-    },
-    onColumnOrderChange: setColumnOrder,
-    onColumnVisibilityChange: setColumnVisibility,
-  };
+  const options = useMemo(
+    () => ({
+      state: {
+        columnOrder,
+        columnVisibility,
+      },
+      onColumnOrderChange: setColumnOrder,
+      onColumnVisibilityChange: setColumnVisibility,
+    }),
+    [
+      JSON.stringify(columnOrder),
+      JSON.stringify(columnVisibility),
+      setColumnOrder,
+      setColumnVisibility,
+    ],
+  );
 
   return options;
 }
