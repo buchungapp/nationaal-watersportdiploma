712147a884f9ccc9623e893fcaa5ba5d7ffa727b
Maurits Misana <maurits@techmaus.nl>
2024-08-02 18:55:22 +0200

    add well-known route

---
+++ apps/web/src/app/.well-known/vercel/flags/route.ts
@@ -0,0 +1,14 @@
+import { verifyAccess, type ApiData } from "@vercel/flags";
+import { unstable_getProviderData as getProviderData } from "@vercel/flags/next";
+import { NextResponse, type NextRequest } from "next/server";
+import * as flags from "../../../../lib/flags";
+
+export const runtime = "edge";
+export const dynamic = "force-dynamic"; // defaults to auto
+
+export async function GET(request: NextRequest) {
+  const access = await verifyAccess(request.headers.get("Authorization"));
+  if (!access) return NextResponse.json(null, { status: 401 });
+
+  return NextResponse.json<ApiData>(getProviderData(flags));
+}
