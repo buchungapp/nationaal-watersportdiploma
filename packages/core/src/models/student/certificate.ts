837f433e8416ed1d4b533848681243663fa5d504
U-THOMASB\thoma <t.ferb1@gmail.com>
2024-08-14 14:29:16 +0200

    feat: check if person data is complete on certificate issueing

--- packages/core/src/models/student/certificate.ts
+++ packages/core/src/models/student/certificate.ts
@@ -1,10 +1,11 @@
 import { schema as s } from '@nawadi/db'
-import { and, eq, isNull } from 'drizzle-orm'
+import { and, eq, exists, isNull, sql } from 'drizzle-orm'
 import { customAlphabet } from 'nanoid'
 import { z } from 'zod'
 import { useQuery } from '../../contexts/index.js'
 import {
   singleOrArray,
+  singleRow,
   successfulCreateResponse,
   uuidSchema,
   withZod,
@@ -95,6 +96,37 @@ export const completeCertificate = withZod(
   z.void(),
   async (input) => {
     const query = useQuery()
+
+    const person = await query
+      .select({
+        firstName: s.person.firstName,
+        lastName: s.person.lastName,
+        dateOfBirth: s.person.dateOfBirth,
+        birthCity: s.person.birthCity,
+      })
+      .from(s.person)
+      .where(
+        exists(
+          query
+            .select({ id: sql`1` })
+            .from(s.certificate)
+            .innerJoin(
+              s.studentCurriculum,
+              eq(s.studentCurriculum.id, s.certificate.studentCurriculumId),
+            )
+            .where(
+              and(
+                eq(s.certificate.id, input.certificateId),
+                eq(s.person.id, s.studentCurriculum.personId),
+              ),
+            ),
+        ),
+      )
+      .then(singleRow)
+
+    if (!person.lastName || !person.dateOfBirth || !person.birthCity) {
+      throw new Error('Person data incomplete')
+    }
 
     const [res] = await query
       .update(s.certificate)
