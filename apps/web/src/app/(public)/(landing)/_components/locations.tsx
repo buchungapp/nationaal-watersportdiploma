766883afb8f383b6aa9c93ffd75aa6648314c49c
Thomas (DJ1TJOO) <44841260+DJ1TJOO@users.noreply.github.com>
2024-08-14 14:00:29 +0200

    fix: location card safari overflow and height (#276)

--- apps/web/src/app/(public)/(landing)/_components/locations.tsx
+++ apps/web/src/app/(public)/(landing)/_components/locations.tsx
@@ -7,27 +7,33 @@ export default async function Locations() {
   const locations = await retrieveLocations();
 
   return (
-    <section className="grid w-full items-center overflow-hidden rounded-[3rem] bg-branding-orange lg:grid-cols-2">
-      <div className="px-4 py-12 text-white lg:p-16">
-        <div className="flex items-center gap-x-3 font-bold uppercase text-white">
-          Vaarlocaties
-          <Double />
-        </div>
-        <h3 className="mt-1.5 text-2xl font-bold text-white">
-          Vind jouw NWD vaarschool.
-        </h3>
-        <p className="mt-2.5 text-orange-100">
-          NWD vaarlocaties voldoen aan de strengste kwaliteitseisen op gebied
-          van persoonlijke- en sociale veiligheid. Verder hebben zij zich
-          gecommitteerd aan de visie van het NWD: veiligheid, kwaliteit en
-          plezier op het water. Daarom zijn zij gelicenseerd om het NWD-diploma
-          uit te geven.
-        </p>
+    // Z-index is required to make the overflow-hidden work on safari
+    <section className="grid w-full overflow-hidden z-0 rounded-[3rem] bg-branding-orange lg:grid-cols-2">
+      <div className="flex h-full items-center">
+        <div className="px-4 py-12 text-white lg:p-16">
+          <div className="flex items-center gap-x-3 font-bold uppercase text-white">
+            Vaarlocaties
+            <Double />
+          </div>
+          <h3 className="mt-1.5 text-2xl font-bold text-white">
+            Vind jouw NWD vaarschool.
+          </h3>
+          <p className="mt-2.5 text-orange-100">
+            NWD vaarlocaties voldoen aan de strengste kwaliteitseisen op gebied
+            van persoonlijke- en sociale veiligheid. Verder hebben zij zich
+            gecommitteerd aan de visie van het NWD: veiligheid, kwaliteit en
+            plezier op het water. Daarom zijn zij gelicenseerd om het
+            NWD-diploma uit te geven.
+          </p>
 
-        <div className="mt-8 flex items-center gap-x-6">
-          <BoxedButton href="/vaarlocaties" className="bg-white text-gray-900">
-            Bekijk NWD vaarlocaties
-          </BoxedButton>
+          <div className="mt-8 flex items-center gap-x-6">
+            <BoxedButton
+              href="/vaarlocaties"
+              className="bg-white text-gray-900"
+            >
+              Bekijk NWD vaarlocaties
+            </BoxedButton>
+          </div>
         </div>
       </div>
       <div className="h-full min-h-[24rem] w-full">
