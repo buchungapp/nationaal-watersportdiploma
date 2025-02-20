1e41da2845bdc33be937bc4ec690117fb2c6a843
Thomas (DJ1TJOO) <t.ferb1@gmail.com>
2025-02-20 20:36:50 +0100

    feat: natural language inputs for date and time seperatly

--- apps/web/src/app/(dashboard)/_components/natural-language-input.tsx
+++ apps/web/src/app/(dashboard)/_components/natural-language-input.tsx
@@ -78,3 +78,153 @@ export default function SmartDatetimePicker({
     </>
   );
 }
+
+export function SmartDatePicker({
+  name,
+  required,
+  invalid,
+  defaultValue,
+}: {
+  name: string;
+  required?: boolean;
+  invalid?: boolean;
+  defaultValue?: Date;
+}) {
+  const dateRef = React.useRef<HTMLInputElement>(null);
+  const inputRef = React.useRef<HTMLInputElement>(null);
+  const [dateTime, setDateTime] = React.useState<Date | null>(
+    defaultValue ?? null,
+  );
+
+  return (
+    <>
+      <Input
+        ref={inputRef}
+        type="text"
+        placeholder='bv. "over 2 weken" of "morgen"'
+        defaultValue={dateTime ? dateTimeUtils.formatDate(dateTime) : ""}
+        invalid={invalid}
+        onBlur={(e) => {
+          // parse the date string when the input field loses focus
+          if (e.target.value.length > 0) {
+            const parsedDateTime = dateTimeUtils.stripTime(
+              dateTimeUtils.parseDateTime(e.target.value),
+            );
+
+            if (parsedDateTime) {
+              setDateTime(parsedDateTime);
+              e.target.value = dateTimeUtils.formatDate(parsedDateTime);
+            }
+          }
+        }}
+      />
+
+      <button
+        onClick={() => {
+          dateRef.current?.showPicker();
+          dateRef.current?.focus();
+        }}
+        type="button"
+        className="bg-white text-slate-500 rounded-xs p-1.5 sm:text-sm absolute right-1 bottom-[6px] sm:bottom-[4px]"
+      >
+        <CalendarDaysIcon className="size-5 sm:size-4" />
+      </button>
+
+      <input
+        type="date"
+        ref={dateRef}
+        step={1}
+        tabIndex={-1}
+        required={required}
+        name={name}
+        value={dateTime ? dateTimeUtils.getDateLocal(dateTime) : ""}
+        onChange={(e) => {
+          const expiryDate = dateTimeUtils.stripTime(new Date(e.target.value));
+          setDateTime(expiryDate);
+          // set the formatted date string in the text input field to keep them in sync
+          if (inputRef.current) {
+            inputRef.current.value = dateTimeUtils.formatDate(expiryDate);
+          }
+        }}
+        // we intentionally make the date input field with a width of 0px
+        // to hide the input field
+        className="w-[0px] opacity-0 overflow-hidden border-none bg-transparent text-slate-500 focus:outline-hidden focus:ring-0 sm:text-sm absolute right-1 bottom-[6px] sm:bottom-[2px] mr-[10px]"
+      />
+    </>
+  );
+}
+
+export function SmartTimePicker({
+  name,
+  required,
+  invalid,
+  defaultValue,
+}: {
+  name: string;
+  required?: boolean;
+  invalid?: boolean;
+  defaultValue?: Date;
+}) {
+  const dateRef = React.useRef<HTMLInputElement>(null);
+  const inputRef = React.useRef<HTMLInputElement>(null);
+  const [dateTime, setDateTime] = React.useState<Date | null>(
+    defaultValue ?? null,
+  );
+
+  return (
+    <>
+      <Input
+        ref={inputRef}
+        type="text"
+        placeholder='bv. "nu" of "over 2 uur"'
+        defaultValue={dateTime ? dateTimeUtils.formatTime(dateTime) : ""}
+        invalid={invalid}
+        onBlur={(e) => {
+          // parse the date string when the input field loses focus
+          if (e.target.value.length > 0) {
+            const parsedDateTime = dateTimeUtils.stripDate(
+              dateTimeUtils.parseDateTime(e.target.value),
+            );
+
+            if (parsedDateTime) {
+              setDateTime(parsedDateTime);
+              e.target.value = dateTimeUtils.formatTime(parsedDateTime);
+            }
+          }
+        }}
+      />
+
+      <button
+        onClick={() => {
+          dateRef.current?.showPicker();
+          dateRef.current?.focus();
+        }}
+        type="button"
+        className="bg-white text-slate-500 rounded-xs p-1.5 sm:text-sm absolute right-1 bottom-[6px] sm:bottom-[4px]"
+      >
+        <CalendarDaysIcon className="size-5 sm:size-4" />
+      </button>
+
+      <input
+        type="time"
+        ref={dateRef}
+        step={1}
+        tabIndex={-1}
+        required={required}
+        name={name}
+        value={dateTime ? dateTimeUtils.getTimeLocal(dateTime) : ""}
+        onChange={(e) => {
+          const expiryDate = dateTimeUtils.stripDate(new Date(e.target.value));
+          setDateTime(expiryDate);
+          // set the formatted date string in the text input field to keep them in sync
+          if (inputRef.current) {
+            inputRef.current.value = dateTimeUtils.formatTime(expiryDate);
+          }
+        }}
+        // we intentionally make the time input field with a width of 0px
+        // to hide the input field
+        className="w-[0px] opacity-0 overflow-hidden border-none bg-transparent text-slate-500 focus:outline-hidden focus:ring-0 sm:text-sm absolute right-1 bottom-[6px] sm:bottom-[2px] mr-[10px]"
+      />
+    </>
+  );
+}
--- packages/lib/src/date-time.ts
+++ packages/lib/src/date-time.ts
@@ -16,6 +16,37 @@ export const formatDateTime = (datetime: Date | string) => {
   });
 };
 
+export const formatDate = (datetime: Date | string) => {
+  return new Date(datetime).toLocaleDateString("nl", {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  });
+};
+
+export const stripTime = (datetime: Date) => {
+  return new Date(datetime.toDateString());
+};
+
+export const formatTime = (datetime: Date | string) => {
+  return new Date(datetime).toLocaleTimeString("nl", {
+    hour: "numeric",
+    minute: "numeric",
+  });
+};
+
+export const stripDate = (datetime: Date) => {
+  return new Date(
+    0,
+    0,
+    0,
+    datetime.getHours(),
+    datetime.getMinutes(),
+    datetime.getSeconds(),
+    datetime.getMilliseconds(),
+  );
+};
+
 export const getDateTimeLocal = (timestamp?: Date): string => {
   const d = timestamp ? new Date(timestamp) : new Date();
   if (d.toString() === "Invalid Date") return "";
@@ -24,4 +55,27 @@ export const getDateTimeLocal = (timestamp?: Date): string => {
     .split(":")
     .slice(0, 2)
     .join(":");
+};
+
+export const getDateLocal = (timestamp?: Date): string => {
+  const d = timestamp ? new Date(timestamp) : new Date();
+  if (d.toString() === "Invalid Date") return "";
+  return (
+    new Date(d.getTime() - d.getTimezoneOffset() * 60000)
+      .toISOString()
+      .split("T")[0] ?? ""
+  );
+};
+
+export const getTimeLocal = (timestamp?: Date): string => {
+  const d = timestamp ? new Date(timestamp) : new Date();
+  if (d.toString() === "Invalid Date") return "";
+  return (
+    new Date(d.getTime() - d.getTimezoneOffset() * 60000)
+      .toISOString()
+      .split("T")[1]
+      ?.split(":")
+      .slice(0, 2)
+      .join(":") ?? ""
+  );
 };
