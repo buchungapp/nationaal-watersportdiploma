c0a0e38f7b33012ef08ae48b0f781a74ff385584
Thomas (DJ1TJOO) <t.ferb1@gmail.com>
2025-02-23 19:33:37 +0100

    feat: update and get external certificate by id

--- packages/core/src/models/certificate/external.ts
+++ packages/core/src/models/certificate/external.ts
@@ -12,6 +12,69 @@ import {
 import { Platform } from "../index.js";
 import { outputSchema } from "../platform/media.schema.js";
 
+export const byId = withZod(
+  z.object({
+    id: uuidSchema,
+  }),
+  z.object({
+    id: uuidSchema,
+    createdAt: z.string().datetime(),
+    identifier: z.string().nullable(),
+    location: z.string().nullable(),
+    awardedAt: z.string().datetime().nullable(),
+    issuingAuthority: z.string().nullable(),
+    title: z.string(),
+    mediaId: z.string().nullable(),
+    metadata: z.record(z.string(), z.string()).nullable(),
+    media: outputSchema.nullable(),
+  }),
+  async (input) => {
+    const query = useQuery();
+
+    const { external_certificate, location } = await query
+      .select()
+      .from(s.externalCertificate)
+      .leftJoin(s.location, eq(s.location.id, s.externalCertificate.locationId))
+      .where(
+        and(
+          eq(s.externalCertificate.id, input.id),
+          isNull(s.externalCertificate.deletedAt),
+        ),
+      )
+      .then(singleRow);
+
+    let media = null;
+    if (external_certificate.mediaId) {
+      media = await Platform.Media.fromId(external_certificate.mediaId);
+
+      if (media) {
+        media.url = await Platform.Media.createSignedUrl({
+          id: external_certificate.mediaId,
+        });
+      }
+    }
+
+    return {
+      id: external_certificate.id,
+      createdAt: dayjs(external_certificate.createdAt).toISOString(),
+      awardedAt: external_certificate.awardedAt
+        ? dayjs(external_certificate.awardedAt).toISOString()
+        : null,
+      location: location
+        ? location.name
+        : (external_certificate.issuingLocation ?? null),
+      identifier: external_certificate.identifier,
+      issuingAuthority: external_certificate.issuingAuthority,
+      title: external_certificate.title,
+      mediaId: external_certificate.mediaId,
+      additionalComments: external_certificate.additionalComments,
+      // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+      metadata: external_certificate._metadata as any,
+      media,
+    };
+  },
+);
+
 export const listForPerson = withZod(
   z.object({
     personId: uuidSchema,
@@ -116,5 +179,44 @@ export const create = withZod(
       });
 
     return singleRow(rows);
+  },
+);
+
+export const update = withZod(
+  z.object({
+    id: uuidSchema,
+    identifier: z.string().nullable().optional(),
+    awardedAt: z.string().date().nullable().optional(),
+    issuingAuthority: z.string().nullable().optional(),
+    issuingLocation: z.string().nullable().optional(),
+    title: z.string().optional(),
+    mediaId: uuidSchema.nullable().optional(),
+    additionalComments: z.string().nullable().optional(),
+  }),
+  successfulCreateResponse,
+  async (input) => {
+    const query = useQuery();
+
+    const row = await query
+      .update(s.externalCertificate)
+      .set({
+        identifier: input.identifier,
+        awardedAt: input.awardedAt,
+        issuingAuthority: input.issuingAuthority,
+        issuingLocation: input.issuingLocation,
+        title: input.title,
+        mediaId: input.mediaId,
+        additionalComments: input.additionalComments,
+      })
+      .where(
+        and(
+          eq(s.externalCertificate.id, input.id),
+          isNull(s.externalCertificate.deletedAt),
+        ),
+      )
+      .returning({ id: s.externalCertificate.id })
+      .then(singleRow);
+
+    return row;
   },
 );
