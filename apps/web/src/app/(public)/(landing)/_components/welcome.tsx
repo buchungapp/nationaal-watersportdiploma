0f3e6043732b7e79c88a5ed2813fc60ac6185cac
Thomas (DJ1TJOO) <t.ferb1@gmail.com>
2025-03-17 16:00:38 +0100

    chore: update landing page welcome links

--- apps/web/src/app/(public)/(landing)/_components/welcome.tsx
+++ apps/web/src/app/(public)/(landing)/_components/welcome.tsx
@@ -3,32 +3,32 @@ import MuxVideo from "./mux-video";
 
 export default function Welcome() {
   return (
-    <div className="grid grid-cols-1 items-center gap-8 px-4 lg:grid-cols-2 lg:px-16">
-      <div className="grid justify-center gap-10 lg:justify-start">
-        <div className="grid gap-6 text-white text-center sm:text-left">
-          <h1 className="max-w-lg text-4xl font-bold lg:text-5xl xl:text-6xl">
+    <div className="items-center gap-8 grid grid-cols-1 lg:grid-cols-2 px-4 lg:px-16">
+      <div className="justify-center lg:justify-start gap-10 grid">
+        <div className="gap-6 grid text-white sm:text-left text-center">
+          <h1 className="max-w-lg font-bold text-4xl lg:text-5xl xl:text-6xl">
             Welkom bij het Nationaal Watersportdiploma.
           </h1>
-          <p className="text-xl text-slate-100">
+          <p className="text-slate-100 text-xl">
             Wij zijn klaar voor het nieuwe vaarseizoen, jij ook?
           </p>
         </div>
-        <div className="flex flex-col items-center sm:items-start gap-x-6 gap-y-4 sm:flex-row">
-          <BoxedButton
-            href="/actueel/RLjvQDiv-lancering-nationaal-watersportdiploma"
-            className="bg-white text-branding-dark"
-          >
-            Lees de aankondiging
-          </BoxedButton>
+        <div className="flex sm:flex-row flex-col items-center sm:items-start gap-x-6 gap-y-4">
           <BoxedButton
             href="/vaarlocaties"
-            className="text-white hover:bg-white/10"
+            className="bg-white text-branding-dark"
           >
             Vind een NWD vaarschool
           </BoxedButton>
+          <BoxedButton
+            href="/diplomalijn/consument"
+            className="hover:bg-white/10 text-white"
+          >
+            Over de NWD diploma's
+          </BoxedButton>
         </div>
       </div>
-      <div className="relative aspect-4/3 w-full overflow-hidden rounded-3xl">
+      <div className="relative rounded-3xl w-full aspect-4/3 overflow-hidden">
         <MuxVideo
           style={{
             position: "absolute",
