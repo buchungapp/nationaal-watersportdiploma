8c7405e4df5b76bb9b92d7ebb215526e46ef96e9
Thomas (DJ1TJOO) <t.ferb1@gmail.com>
2025-03-17 16:00:38 +0100

    chore: update landing page welcome links

--- apps/web/src/app/(public)/(landing)/_components/welcome.tsx
+++ apps/web/src/app/(public)/(landing)/_components/welcome.tsx
@@ -1,25 +1,12 @@
-import { MegaphoneIcon } from "@heroicons/react/16/solid";
-import Link from "next/link";
 import { BoxedButton } from "~/app/(public)/_components/style/buttons";
 import MuxVideo from "./mux-video";
 
 export default function Welcome() {
   return (
-    <div className="grid grid-cols-1 items-center gap-8 px-4 lg:grid-cols-2 lg:px-16">
-      <div className="grid justify-center gap-10 lg:justify-start">
-        <div className="grid gap-6 text-white text-center sm:text-left">
-          <Link
-            href="/actueel/jh38UnmG-persbericht-samenwerking-watersportverbond-en-nationaal-watersportdiploma"
-            className="w-fit rounded-full bg-white/15 px-6 py-2 text-sm text-white 
-            hover:bg-white/25 transition-colors duration-200 backdrop-blur-sm
-            flex items-center gap-2 mb-4 font-medium leading-tight mx-auto sm:mx-0
-            border border-white/20 shadow-[0_0_15px_rgba(255,255,255,0.1)]"
-          >
-            <MegaphoneIcon className="h-4 w-4" /> Lees: Vernieuwd
-            watersportdiploma voor Nederland
-            <span className="text-xs">→</span>
-          </Link>
-          <h1 className="max-w-lg text-4xl font-bold lg:text-5xl xl:text-6xl">
+    <div className="items-center gap-8 grid grid-cols-1 lg:grid-cols-2 px-4 lg:px-16">
+      <div className="justify-center lg:justify-start gap-10 grid">
+        <div className="gap-6 grid text-white sm:text-left text-center">
+          <h1 className="max-w-lg font-bold text-4xl lg:text-5xl xl:text-6xl">
             Welkom bij het Nationaal Watersportdiploma.
           </h1>
           <p className="text-slate-100 text-xl">
@@ -36,9 +23,9 @@ export default function Welcome() {
           </BoxedButton>
           <BoxedButton
             href="/diplomalijn/consument"
-            className="text-white hover:bg-white/10"
+            className="hover:bg-white/10 text-white"
           >
-            Ontdek de diplomalijn
+            Over de NWD diploma's
           </BoxedButton>
         </div>
       </div>
