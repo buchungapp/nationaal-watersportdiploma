3473e8113f6fb0209b0a09917b0f8980b1ac5650
Thomas (DJ1TJOO) <t.ferb1@gmail.com>
2025-03-18 15:34:09 +0100

    chore: add sections to cashback form

--- apps/web/src/app/(public)/cashback/_components/cashback-form.tsx
+++ apps/web/src/app/(public)/cashback/_components/cashback-form.tsx
@@ -3,13 +3,22 @@ import {
   Checkbox,
   CheckboxField,
 } from "~/app/(dashboard)/_components/checkbox";
-import { Field, Label } from "~/app/(dashboard)/_components/fieldset";
+import {
+  Field,
+  Fieldset,
+  Label,
+  Legend,
+} from "~/app/(dashboard)/_components/fieldset";
 import { Input } from "~/app/(dashboard)/_components/input";
+import { Listbox, ListboxOption } from "~/app/(dashboard)/_components/listbox";
 import { MediaDropzone } from "~/app/(dashboard)/_components/media-dropzone";
 import { Textarea } from "~/app/(dashboard)/_components/textarea";
+import { listAllLocations } from "~/lib/nwd";
 import { SubmitButton } from "./cashback-form-client";
 
-export default function CashbackForm() {
+export default async function CashbackForm() {
+  const locations = await listAllLocations();
+
   return (
     <section className="flex flex-col items-center gap-8 bg-branding-light px-4 lg:px-16 py-20 rounded-[3rem] w-full">
       <div className="flex flex-col gap-4">
@@ -24,103 +33,147 @@ export default function CashbackForm() {
         </p>
       </div>
       <form className="@container/cashback-fields bg-white p-4 lg:p-8 rounded-xl w-full max-w-5xl">
-        <div className="gap-8 @2xl/cashback-fields:gap-x-4 grid grid-cols-1 @xl/cashback-fields:grid-cols-12">
-          <Field className="@2xl/cashback-fields:col-span-4 @xl/cashback-fields:col-span-6">
-            <Label>
-              Naam <span className="text-branding-orange">*</span>
-            </Label>
-            <Input name="name" placeholder="Volledige naam" required />
-          </Field>
+        <div className="gap-8 grid grid-cols-1">
+          <Fieldset>
+            <Legend>Persoonlijke gegevens</Legend>
+            <div className="gap-8 @2xl/cashback-fields:gap-x-4 grid grid-cols-1 @xl/cashback-fields:grid-cols-12">
+              <Field className="@2xl/cashback-fields:col-span-4 @xl/cashback-fields:col-span-6">
+                <Label>
+                  Naam <span className="text-branding-orange">*</span>
+                </Label>
+                <Input name="fullName" placeholder="Volledige naam" required />
+              </Field>
 
-          <Field className="@2xl/cashback-fields:col-span-4 @xl/cashback-fields:col-span-6">
-            <Label>
-              E-mail <span className="text-branding-orange">*</span>
-            </Label>
-            <Input
-              name="email"
-              type="email"
-              placeholder="naam@voorbeeld.nl"
-              required
-            />
-          </Field>
+              <Field className="@2xl/cashback-fields:col-span-4 @xl/cashback-fields:col-span-6">
+                <Label>
+                  E-mail <span className="text-branding-orange">*</span>
+                </Label>
+                <Input
+                  name="email"
+                  type="email"
+                  placeholder="naam@voorbeeld.nl"
+                  required
+                />
+              </Field>
 
-          <Field className="@2xl/cashback-fields:col-span-4 @xl/cashback-fields:col-span-6">
-            <Label>
-              Telefoonnummer <span className="text-branding-orange">*</span>
-            </Label>
-            <Input name="phone" type="tel" placeholder="06 12345678" required />
-          </Field>
+              <Field className="@2xl/cashback-fields:col-span-4 @xl/cashback-fields:col-span-6">
+                <Label>
+                  Telefoonnummer <span className="text-branding-orange">*</span>
+                </Label>
+                <Input
+                  name="phone"
+                  type="tel"
+                  placeholder="06 12345678"
+                  required
+                />
+              </Field>
 
-          <Field className="@2xl/cashback-fields:col-span-6 @xl/cashback-fields:col-span-6">
-            <Label>
-              Adres <span className="text-branding-orange">*</span>
-            </Label>
-            <Input
-              name="address"
-              placeholder="Straatnaam en huisnummer"
-              required
-            />
-          </Field>
+              <Field className="@2xl/cashback-fields:col-span-6 @xl/cashback-fields:col-span-6">
+                <Label>
+                  Adres <span className="text-branding-orange">*</span>
+                </Label>
+                <Input
+                  name="address"
+                  placeholder="Straatnaam en huisnummer"
+                  required
+                />
+              </Field>
 
-          <Field className="@2xl/cashback-fields:col-span-3 @xl/cashback-fields:col-span-6">
-            <Label>
-              Postcode <span className="text-branding-orange">*</span>
-            </Label>
-            <Input name="postalCode" placeholder="1234 AB" required />
-          </Field>
+              <Field className="@2xl/cashback-fields:col-span-3 @xl/cashback-fields:col-span-6">
+                <Label>
+                  Postcode <span className="text-branding-orange">*</span>
+                </Label>
+                <Input name="postalCode" placeholder="1234 AB" required />
+              </Field>
 
-          <Field className="@2xl/cashback-fields:col-span-3 @xl/cashback-fields:col-span-6">
-            <Label>
-              Plaats <span className="text-branding-orange">*</span>
-            </Label>
-            <Input name="city" placeholder="Plaatsnaam" required />
-          </Field>
+              <Field className="@2xl/cashback-fields:col-span-3 @xl/cashback-fields:col-span-6">
+                <Label>
+                  Plaats <span className="text-branding-orange">*</span>
+                </Label>
+                <Input name="city" placeholder="Plaatsnaam" required />
+              </Field>
+            </div>
+          </Fieldset>
+          <Fieldset>
+            <Legend>X cursus 2024</Legend>
+            <div className="gap-8 @2xl/cashback-fields:gap-x-4 grid grid-cols-1 @xl/cashback-fields:grid-cols-12">
+              <Field className="@2xl/cashback-fields:col-span-5 @xl/cashback-fields:col-span-6">
+                <Label>
+                  Vaarlocatie behaald{" "}
+                  <span className="text-branding-orange">*</span>
+                </Label>
+                <Input
+                  name="verificationLocation"
+                  placeholder="Naam vaarlocatie"
+                  required
+                />
+              </Field>
 
-          <Field className="@2xl/cashback-fields:col-span-4 @xl/cashback-fields:col-span-6">
-            <Label>
-              IBAN Rekeningnummer{" "}
-              <span className="text-branding-orange">*</span>
-            </Label>
-            <Input name="iban" placeholder="NL12 ABCD 1234 5678 90" required />
-          </Field>
+              <Field className="@xl/cashback-fields:col-span-12">
+                <Label>
+                  Upload @TODO <span className="text-branding-orange">*</span>
+                </Label>
+                <MediaDropzone name="verificationMedia" />
+              </Field>
+            </div>
+          </Fieldset>
+          <Fieldset>
+            <Legend>Geboekte cursus bij NWD vaarlocatie</Legend>
+            <div className="gap-8 @2xl/cashback-fields:gap-x-4 grid grid-cols-1 @xl/cashback-fields:grid-cols-12">
+              <Field className="@2xl/cashback-fields:col-span-5 @xl/cashback-fields:col-span-6">
+                <Label>
+                  Vaarlocatie <span className="text-branding-orange">*</span>
+                </Label>
+                <Listbox name="primaryRole" className="w-full">
+                  {locations.map((location) => (
+                    <ListboxOption key={location.id} value={location.id}>
+                      {location.name}
+                    </ListboxOption>
+                  ))}
+                </Listbox>
+              </Field>
 
-          <Field className="@2xl/cashback-fields:col-span-5 @xl/cashback-fields:col-span-6">
-            <Label>
-              Vaarschool <span className="text-branding-orange">*</span>
-            </Label>
-            <Input
-              name="sailingSchool"
-              placeholder="Naam vaarschool"
-              required
-            />
-          </Field>
+              <Field className="@2xl/cashback-fields:col-span-3 @xl/cashback-fields:col-span-6">
+                <Label>
+                  Boekingsnummer <span className="text-branding-orange">*</span>
+                </Label>
+                <Input
+                  name="bookingNumber"
+                  placeholder="Boekingsnummer"
+                  required
+                />
+              </Field>
+            </div>
+          </Fieldset>
+          <Fieldset>
+            <Legend>Bank gegevens</Legend>
+            <div className="gap-8 @2xl/cashback-fields:gap-x-4 grid grid-cols-1 @xl/cashback-fields:grid-cols-12">
+              <Field className="@xl/cashback-fields:col-span-6">
+                <Label>
+                  IBAN Rekeningnummer{" "}
+                  <span className="text-branding-orange">*</span>
+                </Label>
+                <Input
+                  name="iban"
+                  placeholder="NL12 ABCD 1234 5678 90"
+                  required
+                />
+              </Field>
 
-          <Field className="@2xl/cashback-fields:col-span-3 @xl/cashback-fields:col-span-6">
-            <Label>
-              Boekingsnummer <span className="text-branding-orange">*</span>
-            </Label>
-            <Input name="bookingNumber" placeholder="Boekingsnummer" required />
-          </Field>
+              <Field className="@xl/cashback-fields:col-span-12">
+                <Label>Opmerking</Label>
+                <Textarea name="comments" placeholder="Eventuele opmerkingen" />
+              </Field>
 
-          <Field className="@xl/cashback-fields:col-span-12">
-            <Label>
-              Upload @TODO <span className="text-branding-orange">*</span>
-            </Label>
-            <MediaDropzone name="media" />
-          </Field>
-
-          <Field className="@xl/cashback-fields:col-span-12">
-            <Label>Opmerking</Label>
-            <Textarea name="comments" placeholder="Eventuele opmerkingen" />
-          </Field>
-
-          <CheckboxField className="@xl/cashback-fields:col-span-12">
-            <Label>
-              Ik ga akkoord met de voorwaarden{" "}
-              <span className="text-branding-orange">*</span>
-            </Label>
-            <Checkbox name="terms" />
-          </CheckboxField>
+              <CheckboxField className="@xl/cashback-fields:col-span-12">
+                <Label>
+                  Ik ga akkoord met de voorwaarden{" "}
+                  <span className="text-branding-orange">*</span>
+                </Label>
+                <Checkbox name="terms" />
+              </CheckboxField>
+            </div>
+          </Fieldset>
         </div>
 
         <div className="flex justify-end mt-8">
