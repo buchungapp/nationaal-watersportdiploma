b7d0da09b9aaab078ffae4d204fe61d828590de8
Maurits Misana <maurits@techmaus.nl>
2025-03-20 22:14:39 +0100

    add notification pill

--- apps/web/src/app/(public)/(landing)/_components/welcome.tsx
+++ apps/web/src/app/(public)/(landing)/_components/welcome.tsx
@@ -1,3 +1,5 @@
+import { MegaphoneIcon } from "@heroicons/react/16/solid";
+import Link from "next/link";
 import { BoxedButton } from "~/app/(public)/_components/style/buttons";
 import MuxVideo from "./mux-video";
 
@@ -6,6 +8,17 @@ export default function Welcome() {
     <div className="grid grid-cols-1 items-center gap-8 px-4 lg:grid-cols-2 lg:px-16">
       <div className="grid justify-center gap-10 lg:justify-start">
         <div className="grid gap-6 text-white text-center sm:text-left">
+          <Link
+            href="/actueel/jh38UnmG-persbericht-samenwerking-watersportverbond-en-nationaal-watersportdiploma"
+            className="w-fit rounded-full bg-white/15 px-6 py-2 text-sm text-white 
+            hover:bg-white/25 transition-colors duration-200 backdrop-blur-sm
+            flex items-center gap-2 mb-4 font-medium leading-tight mx-auto sm:mx-0
+            border border-white/20 shadow-[0_0_15px_rgba(255,255,255,0.1)]"
+          >
+            <MegaphoneIcon className="h-4 w-4" /> Het Watersportverbond kiest
+            voor NWD als nieuwe standaard
+            <span className="text-xs">→</span>
+          </Link>
           <h1 className="max-w-lg text-4xl font-bold lg:text-5xl xl:text-6xl">
             Welkom bij het Nationaal Watersportdiploma.
           </h1>
