53d77a7a226acc7317ae680093e3bcd4381624ae
Maurits Misana <maurits@techmaus.nl>
2025-04-08 15:02:24 +0200

    more reporting

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -4,7 +4,7 @@ const cspHeader = `
     default-src 'self';
     script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdn.usefathom.com https://maps.googleapis.com https://vercel.live;
     style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://vercel.live;
-    img-src 'self' blob: data: https://*.mux.com https://*.googleapis.com https://*.gstatic.com https://vercel.live https://vercel.com;
+    img-src 'self' blob: data: https://*.mux.com https://*.googleapis.com https://*.gstatic.com https://vercel.live https://vercel.com https://cdn.usefathom.com;
     font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com https://vercel.live https://assets.vercel.com;
     connect-src 'self' 
         https://cdn.usefathom.com
