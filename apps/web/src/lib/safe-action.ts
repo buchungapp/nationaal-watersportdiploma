d5b0dab6f457394683bbf00b9f171d39a6493e0c
Thomas (DJ1TJOO) <t.ferb1@gmail.com>
2025-04-29 13:49:19 +0200

    feat: safe action client

--- apps/web/src/lib/safe-action.ts
+++ apps/web/src/lib/safe-action.ts
@@ -1,3 +1,30 @@
 import { createSafeActionClient } from "next-safe-action";
+import { z } from "zod";
 
-export const actionClient = createSafeActionClient();
+const DEFAULT_SERVER_ERROR_MESSAGE =
+  "Er is iets misgegaan. Probeer het later opnieuw.";
+
+export const actionClient = createSafeActionClient({
+  handleServerError(e) {
+    if (e instanceof Error) {
+      return e.message;
+    }
+
+    return DEFAULT_SERVER_ERROR_MESSAGE;
+  },
+});
+
+export const actionClientWithMeta = createSafeActionClient({
+  handleServerError(e) {
+    if (e instanceof Error) {
+      return e.message;
+    }
+
+    return DEFAULT_SERVER_ERROR_MESSAGE;
+  },
+  defineMetadataSchema() {
+    return z.object({
+      name: z.string(),
+    });
+  },
+});
