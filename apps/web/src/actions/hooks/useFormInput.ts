af728ef1a7137cd866f671d6603c374a7f632a29
Thomas (DJ1TJOO) <t.ferb1@gmail.com>
2025-05-05 11:17:30 +0200

    feat: allow default values

--- apps/web/src/actions/hooks/useFormInput.ts
+++ apps/web/src/actions/hooks/useFormInput.ts
@@ -1,7 +1,6 @@
-type FormDataLikeInput = {
-  [Symbol.iterator](): IterableIterator<[string, FormDataEntryValue]>;
-  entries(): IterableIterator<[string, FormDataEntryValue]>;
-};
+import type { FormDataLikeInput } from "../safe-action";
+
+type Nullable<T> = { [K in keyof T]: T[K] | null };
 
 type Input<T extends Record<string, unknown>> =
   | FormData
@@ -28,41 +27,58 @@ function isFormDataLikeInput<T extends Record<string, unknown>>(
  */
 export const useFormInput = <T extends Record<string, unknown>>(
   input: Input<T>,
+  defaultValue?: Nullable<Partial<T>>,
 ) => {
-  const getInputValue = (name: keyof T) => {
-    if (input === undefined) return undefined;
+  const returnValue = <K>(name: keyof T) =>
+    defaultValue?.[name] as K | undefined;
+  const defaultValueArray = <K>(name: keyof T) =>
+    returnValue<K>(name) ? [returnValue<K>(name)] : undefined;
+
+  /**
+   * @description This function is used to get the input value from the form / execution. If the input is undefined, it will return the default value.
+   * @param name The name of the input
+   * @returns The input value
+   */
+  const getInputValue = <K>(name: keyof T) => {
+    if (input === undefined) return returnValue<K>(name);
 
     if (isFormData(input)) {
-      const value = input.get(name as string) as string | null;
-      return value ?? undefined;
+      const value = input.get(name as string) as K | null;
+      return value ?? returnValue<K>(name);
     }
 
     if (isFormDataLikeInput(input)) {
       for (const [key, value] of input) {
-        if (key === name) return value as string;
+        if (key === name) return value as K;
       }
-      return undefined;
+      return returnValue<K>(name);
     }
 
-    return input[name] as string | undefined;
+    return (input[name] as K | undefined) ?? returnValue<K>(name);
   };
 
-  const getInputValues = (name: keyof T) => {
-    if (input === undefined) return undefined;
+  /**
+   * @description This function is used to get the input values from the form / execution. If the input is undefined, it will return the default values.
+   * @param name The name of the input
+   * @returns The input values
+   */
+  const getInputValues = <K>(name: keyof T) => {
+    if (input === undefined) return defaultValueArray<K>(name);
 
     if (isFormData(input)) {
-      return input.getAll(name as string) as string[];
+      const values = input.getAll(name as string) as K[];
+      return values.length > 0 ? values : defaultValueArray<K>(name);
     }
 
     if (isFormDataLikeInput(input)) {
-      const values: string[] = [];
+      const values: K[] = [];
       for (const [key, value] of input) {
-        if (key === name) values.push(value as string);
+        if (key === name) values.push(value as K);
       }
-      return values.length > 0 ? values : undefined;
+      return values.length > 0 ? values : defaultValueArray<K>(name);
     }
 
-    return input[name] ? [input[name] as string] : undefined;
+    return input[name] ? [input[name] as K] : defaultValueArray<K>(name);
   };
 
   return { getInputValue, getInputValues };
