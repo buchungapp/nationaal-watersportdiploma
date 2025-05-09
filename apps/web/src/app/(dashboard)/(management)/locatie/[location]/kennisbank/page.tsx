58baa6281c29b229cb67d77fc08f6a6e438cb67f
Thomas (DJ1TJOO) <t.ferb1@gmail.com>
2025-05-09 18:10:18 +0200

    feat: kennisbank suspense

--- apps/web/src/app/(dashboard)/(management)/locatie/[location]/kennisbank/page.tsx
+++ apps/web/src/app/(dashboard)/(management)/locatie/[location]/kennisbank/page.tsx
@@ -1,33 +1,15 @@
-import { ArrowDownTrayIcon } from "@heroicons/react/16/solid";
-import { DocumentTextIcon } from "@heroicons/react/20/solid";
-import { connection } from "next/server";
-import prettyBytes from "pretty-bytes";
-import { Badge } from "~/app/(dashboard)/_components/badge";
-import { Button } from "~/app/(dashboard)/_components/button";
 import { Heading } from "~/app/(dashboard)/_components/heading";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "~/app/(dashboard)/_components/table";
 import { Text, TextLink } from "~/app/(dashboard)/_components/text";
-import dayjs from "~/lib/dayjs";
-import { listKnowledgeCenterDocuments } from "~/lib/nwd";
+import { FilesTable } from "./_components/files-table";
 
 export default async function Page(_props: {
   params: Promise<{
     location: string;
   }>;
 }) {
-  await connection();
-  const documents = await listKnowledgeCenterDocuments();
-
   return (
     <>
-      <div className="flex items-end justify-between gap-4">
+      <div className="flex justify-between items-end gap-4">
         <Heading>Kennisbank</Heading>
       </div>
 
@@ -41,58 +23,7 @@ export default async function Page(_props: {
         .
       </Text>
 
-      <Table
-        className="mt-8 [--gutter:--spacing(6)] sm:[--gutter:--spacing(8)]"
-        dense
-      >
-        <TableHead>
-          <TableRow>
-            <TableHeader>Naam</TableHeader>
-            <TableHeader>Laatste update</TableHeader>
-            <TableHeader />
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {documents.map((document) => (
-            <TableRow key={document.id}>
-              <TableCell>
-                <div className="flex items-center gap-4">
-                  <DocumentTextIcon className="size-6 text-zinc-500" />
-                  <div>
-                    <TextLink
-                      href={`/kennisbank/${document.id}`}
-                      target="_blank"
-                      prefetch={false}
-                    >
-                      {document.name}
-                    </TextLink>
-                    <div className="text-zinc-500">
-                      <Badge className="mr-2">
-                        {document.mimeType?.split("/")[1]?.toUpperCase()}
-                      </Badge>
-                      {prettyBytes(document.size, { locale: "nl" })}
-                    </div>
-                  </div>
-                </div>
-              </TableCell>
-              <TableCell className="text-zinc-500">
-                {dayjs(document.updatedAt).fromNow()}
-              </TableCell>
-              <TableCell>
-                <div className="-mx-3 -my-1.5 sm:-mx-2.5">
-                  <Button
-                    plain
-                    href={`/kennisbank/${document.id}?download=1`}
-                    prefetch={false}
-                  >
-                    <ArrowDownTrayIcon />
-                  </Button>
-                </div>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      <FilesTable />
     </>
   );
 }
