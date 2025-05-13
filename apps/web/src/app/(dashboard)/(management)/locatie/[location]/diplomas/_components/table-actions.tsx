8cd631bb02515f4096b78e21c3c0b20b13ef70ae
Thomas (DJ1TJOO) <t.ferb1@gmail.com>
2025-05-13 13:12:47 +0200

    feat: moved diplomas download all to download action, removed unused api route

--- apps/web/src/app/(dashboard)/(management)/locatie/[location]/diplomas/_components/table-actions.tsx
+++ apps/web/src/app/(dashboard)/(management)/locatie/[location]/diplomas/_components/table-actions.tsx
@@ -1,32 +1,181 @@
+"use client";
+import {
+  Disclosure as HeadlessDisclosure,
+  DisclosureButton as HeadlessDisclosureButton,
+  DisclosurePanel as HeadlessDisclosurePanel,
+} from "@headlessui/react";
+import { ChevronRightIcon } from "@heroicons/react/16/solid";
+import { useAction } from "next-safe-action/hooks";
+import { useState } from "react";
+import { useFormStatus } from "react-dom";
+import { toast } from "sonner";
+import {
+  Alert,
+  AlertActions,
+  AlertBody,
+  AlertDescription,
+  AlertTitle,
+} from "~/app/(dashboard)/_components/alert";
+import { Button } from "~/app/(dashboard)/_components/button";
 import {
   DropdownItem,
   DropdownLabel,
 } from "~/app/(dashboard)/_components/dropdown";
+import {
+  Description,
+  Field,
+  Fieldset,
+  Label,
+  Legend,
+} from "~/app/(dashboard)/_components/fieldset";
+import { Subheading } from "~/app/(dashboard)/_components/heading";
+import { Input } from "~/app/(dashboard)/_components/input";
+import {
+  Radio,
+  RadioField,
+  RadioGroup,
+} from "~/app/(dashboard)/_components/radio";
 import { TableSelectionButton } from "~/app/(dashboard)/_components/table-action";
+import { Text } from "~/app/(dashboard)/_components/text";
+import { downloadCertificatesAction } from "~/app/_actions/certificate/download-certificates-action";
+import { useFormInput } from "~/app/_actions/hooks/useFormInput";
+import { DEFAULT_SERVER_ERROR_MESSAGE } from "~/app/_actions/utils";
+import Spinner from "~/app/_components/spinner";
+import dayjs from "~/lib/dayjs";
 import type { listCertificates } from "~/lib/nwd";
 
 type Certificate = Awaited<ReturnType<typeof listCertificates>>[number];
-export function Download({ rows }: { rows: Certificate[] }) {
-  const params = new URLSearchParams();
 
-  for (const row of rows) {
-    params.append("certificate", row.handle);
-  }
+interface Props {
+  rows: Certificate[];
+  resetSelection: () => void;
+}
+
+function DownloadCertificatesDialog({
+  rows,
+  isOpen,
+  close,
+  resetSelection,
+}: Props & {
+  isOpen: boolean;
+  close: () => void;
+}) {
+  const closeDialog = () => {
+    close();
+    reset();
+  };
+
+  const { execute, input, reset } = useAction(
+    downloadCertificatesAction.bind(
+      null,
+      rows.map((row) => row.handle),
+    ),
+    {
+      onSuccess: () => {
+        closeDialog();
+        resetSelection();
+      },
+      onError: () => {
+        toast.error(DEFAULT_SERVER_ERROR_MESSAGE);
+      },
+    },
+  );
+
+  const { getInputValue } = useFormInput(input, {
+    filename: `${dayjs().toISOString()}-export-diplomas`,
+    sort: "student",
+  });
 
   return (
-    <DropdownItem
-      href={`/api/export/certificate/pdf?${params.toString()}`}
-      target="_blank"
-    >
-      <DropdownLabel>Download PDF</DropdownLabel>
-    </DropdownItem>
+    <>
+      <Alert open={isOpen} onClose={closeDialog} size="lg">
+        <AlertTitle>Diploma's downloaden</AlertTitle>
+        <AlertDescription>
+          Download een PDF-bestand met de diploma's van de geselecteerde
+          cursisten.
+        </AlertDescription>
+        <form action={execute}>
+          <AlertBody>
+            <HeadlessDisclosure>
+              <HeadlessDisclosureButton className="flex">
+                <div className="flex justify-center items-center mr-6 h-6">
+                  <ChevronRightIcon className="w-3.5 h-3.5 ui-open:rotate-90 transition-transform shrink-0" />
+                </div>
+                <Subheading>Geavanceerde opties</Subheading>
+              </HeadlessDisclosureButton>
+              <HeadlessDisclosurePanel className="mt-2 pl-10">
+                <Field>
+                  <Label>Bestandsnaam</Label>
+                  <Input
+                    name="filename"
+                    type="text"
+                    required
+                    defaultValue={getInputValue("filename")}
+                  />
+                </Field>
+
+                <Fieldset className="mt-6">
+                  <Legend>Sortering</Legend>
+                  <Text>
+                    Hoe moeten de diploma's in de PDF gesorteerd zijn?
+                  </Text>
+                  <RadioGroup name="sort" defaultValue={getInputValue("sort")}>
+                    <RadioField>
+                      <Radio value="student" />
+                      <Label>Naam cursist</Label>
+                      <Description>Sortering op voornaam, A tot Z.</Description>
+                    </RadioField>
+                    <RadioField>
+                      <Radio value="instructor" />
+                      <Label>Naam instructeur</Label>
+                      <Description>
+                        Sortering op voornaam instructeur, A tot Z. Diploma's
+                        zonder instructeur worden als laatste getoond.
+                      </Description>
+                    </RadioField>
+                  </RadioGroup>
+                </Fieldset>
+              </HeadlessDisclosurePanel>
+            </HeadlessDisclosure>
+          </AlertBody>
+          <AlertActions>
+            <Button plain onClick={closeDialog}>
+              Annuleren
+            </Button>
+            <DownloadSubmitButton />
+          </AlertActions>
+        </form>
+      </Alert>
+    </>
   );
 }
 
-export function ActionButtons({ rows }: { rows: Certificate[] }) {
+function DownloadSubmitButton() {
+  const { pending } = useFormStatus();
   return (
-    <TableSelectionButton>
-      <Download rows={rows} />
-    </TableSelectionButton>
+    <Button color="branding-dark" disabled={pending} type="submit">
+      {pending ? <Spinner className="text-white" /> : null}
+      Download
+    </Button>
+  );
+}
+
+export function ActionButtons(props: Props) {
+  const [isDialogOpen, setIsDialogOpen] = useState<string | null>(null);
+
+  return (
+    <>
+      <TableSelectionButton>
+        <DropdownItem onClick={() => setIsDialogOpen("download")}>
+          <DropdownLabel>Diploma's downloaden</DropdownLabel>
+        </DropdownItem>
+      </TableSelectionButton>
+
+      <DownloadCertificatesDialog
+        {...props}
+        isOpen={isDialogOpen === "download"}
+        close={() => setIsDialogOpen(null)}
+      />
+    </>
   );
 }
