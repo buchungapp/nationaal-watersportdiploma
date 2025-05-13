9b104b997a8b7dcb0c3d033540d331e9d2b2dc7a
Thomas (DJ1TJOO) <t.ferb1@gmail.com>
2025-05-13 17:30:48 +0200

    refactor: catalyst combobox

--- apps/web/src/app/(dashboard)/_components/combobox.tsx
+++ apps/web/src/app/(dashboard)/_components/combobox.tsx
@@ -1,187 +1,142 @@
 "use client";
 
-import {
-  Combobox as HeadlessCombobox,
-  ComboboxButton as HeadlessComboboxButton,
-  ComboboxInput as HeadlessComboboxInput,
-  type ComboboxInputProps as HeadlessComboboxInputProps,
-  ComboboxOption as HeadlessComboboxOption,
-  type ComboboxOptionProps as HeadlessComboboxOptionProps,
-  ComboboxOptions as HeadlessComboboxOptions,
-  type ComboboxOptionsProps as HeadlessComboboxOptionsProps,
-  type ComboboxProps as HeadlessComboboxProps,
-} from "@headlessui/react";
+import * as Headless from "@headlessui/react";
 import clsx from "clsx";
-import type { Fragment } from "react";
-import { useEffect, useState } from "react";
-
-/****************************************************************
- * TODO: this combobox is temporary used should be from catalyst
- ****************************************************************/
+import { useState } from "react";
 
 export function Combobox<T>({
+  options,
+  displayValue,
+  filter,
+  anchor = "bottom",
   className,
   placeholder,
   autoFocus,
   "aria-label": ariaLabel,
-  children: options,
-  onChange: theirOnChange,
-  value: theirValue,
-  displayValue,
-  setQuery,
-  invalid,
+  children,
   ...props
 }: {
+  options: T[];
+  displayValue: (value: T | null) => string | undefined;
+  filter?: (value: T, query: string) => boolean;
   className?: string;
   placeholder?: string;
   autoFocus?: boolean;
   "aria-label"?: string;
-  children?: HeadlessComboboxOptionsProps["children"];
-  setQuery?: (query: string) => void;
-  invalid?: boolean;
-} & Omit<HeadlessComboboxProps<T | null, false, typeof Fragment>, "multiple"> &
-  Pick<HeadlessComboboxInputProps<typeof Fragment, T | null>, "displayValue">) {
-  const [value, setValue] = useState<T | null>(theirValue ?? null);
+  children: (value: NonNullable<T>) => React.ReactElement;
+} & Omit<Headless.ComboboxProps<T, false>, "as" | "multiple" | "children"> & {
+    anchor?: "top" | "bottom";
+  }) {
+  const [query, setQuery] = useState("");
 
-  useEffect(() => {
-    setValue(theirValue ?? null);
-  }, [theirValue]);
+  const filteredOptions =
+    query === ""
+      ? options
+      : options.filter((option) =>
+          filter
+            ? filter(option, query)
+            : displayValue(option)?.toLowerCase().includes(query.toLowerCase()),
+        );
 
   return (
-    <HeadlessCombobox
+    <Headless.Combobox
       {...props}
-      value={value}
-      immediate
-      onChange={(value) => {
-        setQuery?.("");
-        setValue(value);
-        theirOnChange?.(value);
-      }}
-      onClose={() => setQuery?.("")}
       multiple={false}
+      virtual={{ options: filteredOptions }}
+      onClose={() => setQuery("")}
     >
-      <div
+      <span
+        data-slot="control"
         className={clsx([
           className,
-
           // Basic layout
-          "group mt-3 relative block w-full",
-
+          "relative block w-full",
           // Background color + shadow applied to inset pseudo element, so shadow blends with border in light mode
           "before:absolute before:inset-px before:rounded-[calc(var(--radius-lg)-1px)] before:bg-white before:shadow-sm",
-
           // Background color is moved to control and shadow is removed in dark mode so hide `before` pseudo
           "dark:before:hidden",
-
-          // Hide default focus styles
-          "focus:outline-hidden",
-
           // Focus ring
-          "after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:ring-inset after:ring-transparent sm:data-focus:after:ring-2 sm:data-focus:after:ring-blue-500",
-
+          "after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:ring-transparent after:ring-inset sm:focus-within:after:ring-2 sm:focus-within:after:ring-blue-500",
           // Disabled state
-          "data-disabled:opacity-50 data-disabled:before:bg-zinc-950/5 data-disabled:before:shadow-none",
+          "has-data-disabled:opacity-50 has-data-disabled:before:bg-zinc-950/5 has-data-disabled:before:shadow-none",
+          // Invalid state
+          "has-data-invalid:before:shadow-red-500/10",
         ])}
-        {...(invalid && { "data-invalid": true })}
       >
-        <HeadlessComboboxInput
+        <Headless.ComboboxInput
           autoFocus={autoFocus}
           data-slot="control"
           aria-label={ariaLabel}
-          {...(invalid && { "data-invalid": true })}
-          displayValue={displayValue}
+          displayValue={(option: T) => displayValue(option) ?? ""}
+          onChange={(event) => setQuery(event.target.value)}
           placeholder={placeholder}
-          onChange={(e) => setQuery?.(e.target.value)}
-          onBlur={() => setQuery?.("")}
           className={clsx([
+            className,
             // Basic layout
             "relative block w-full appearance-none rounded-lg py-[calc(--spacing(2.5)-1px)] sm:py-[calc(--spacing(1.5)-1px)]",
-
-            // Set minimum height for when no value is selected
-            "min-h-11 sm:min-h-9",
-
             // Horizontal padding
-            "pl-[calc(--spacing(3.5)-1px)] pr-[calc(--spacing(7)-1px)] sm:pl-[calc(--spacing(3)-1px)]",
-
+            "pr-[calc(--spacing(10)-1px)] pl-[calc(--spacing(3.5)-1px)] sm:pr-[calc(--spacing(9)-1px)] sm:pl-[calc(--spacing(3)-1px)]",
             // Typography
-            "text-left text-base/6 text-zinc-950 placeholder:text-zinc-500 sm:text-sm/6 dark:text-white forced-colors:text-[CanvasText]",
-
+            "text-base/6 text-zinc-950 placeholder:text-zinc-500 sm:text-sm/6 dark:text-white",
             // Border
-            "border border-zinc-950/10 group-data-active:border-zinc-950/20 group-data-hover:border-zinc-950/20 dark:border-white/10 dark:group-data-active:border-white/20 dark:group-data-hover:border-white/20",
-
+            "border border-zinc-950/10 data-hover:border-zinc-950/20 dark:border-white/10 dark:data-hover:border-white/20",
             // Background color
             "bg-transparent dark:bg-white/5",
-
+            // Hide default focus styles
+            "focus:outline-hidden",
             // Invalid state
-            "group-data-invalid:border-red-500 group-data-hover:group-data-invalid:border-red-500 dark:group-data-invalid:border-red-600 dark:data-hover:group-data-invalid:border-red-600",
-
+            "data-invalid:border-red-500 data-invalid:data-hover:border-red-500 dark:data-invalid:border-red-500 dark:data-invalid:data-hover:border-red-500",
             // Disabled state
-            "group-data-disabled:border-zinc-950/20 group-data-disabled:opacity-100 dark:group-data-disabled:border-white/15 dark:group-data-disabled:bg-white/[2.5%] dark:group-data-disabled:data-hover:border-white/15",
+            "data-disabled:border-zinc-950/20 dark:data-disabled:border-white/15 dark:data-disabled:bg-white/[2.5%] dark:data-hover:data-disabled:border-white/15",
+            // System icons
+            "dark:scheme-dark",
           ])}
         />
-        <HeadlessComboboxButton
-          className={
-            "absolute inset-y-0 right-0 flex items-center rounded-r-md px-3 focus:outline-hidden"
-          }
-        >
-          <span className="right-0 absolute inset-y-0 flex items-center pr-2 pointer-events-none">
-            <svg
-              className="stroke-zinc-500 dark:stroke-zinc-400 forced-colors:stroke-[CanvasText] group-data-disabled:stroke-zinc-600 size-5 sm:size-4"
-              viewBox="0 0 16 16"
-              aria-hidden="true"
-              fill="none"
-            >
-              <path
-                d="M5.75 10.75L8 13L10.25 10.75"
-                strokeWidth={1.5}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M10.25 5.25L8 3L5.75 5.25"
-                strokeWidth={1.5}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          </span>
-        </HeadlessComboboxButton>
-
-        <HeadlessComboboxOptions
-          as="div"
-          anchor={{
-            to: "bottom start",
-            gap: "var(--anchor-gap)",
-            offset: "var(--anchor-offset)",
-            padding: "var(--anchor-padding)",
-          }}
-          className={clsx(
-            // Anchor positioning
-            "[--anchor-offset:-1.625rem] [--anchor-padding:--spacing(4)] sm:[--anchor-offset:-1.375rem]",
-
-            // Base styles
-            "isolate w-max min-w-[calc(var(--button-width)+var(--input-width)+1.75rem)] empty:invisible border-0 select-none scroll-py-1 rounded-xl p-1",
-
-            // Invisible border that is only visible in `forced-colors` mode for accessibility purposes
-            "outline outline-1 outline-transparent focus:outline-hidden",
-
-            // Handle scrolling when menu won't fit in viewport
-            "overflow-y-scroll overscroll-contain",
-
-            // Popover background
-            "bg-white/75 backdrop-blur-xl dark:bg-zinc-800/75",
-
-            // Shadows
-            "shadow-lg ring-1 ring-zinc-950/10 dark:ring-inset dark:ring-white/10",
-
-            // Transitions
-            "transition duration-100 ease-in data-leave:data-closed:opacity-0",
-          )}
-        >
-          {options}
-        </HeadlessComboboxOptions>
-      </div>
-    </HeadlessCombobox>
+        <Headless.ComboboxButton className="group right-0 absolute inset-y-0 flex items-center px-2">
+          <svg
+            className="stroke-zinc-500 dark:group-data-hover:stroke-zinc-300 dark:stroke-zinc-400 forced-colors:stroke-[CanvasText] group-data-disabled:stroke-zinc-600 group-data-hover:stroke-zinc-700 size-5 sm:size-4"
+            viewBox="0 0 16 16"
+            aria-hidden="true"
+            fill="none"
+          >
+            <path
+              d="M5.75 10.75L8 13L10.25 10.75"
+              strokeWidth={1.5}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M10.25 5.25L8 3L5.75 5.25"
+              strokeWidth={1.5}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </Headless.ComboboxButton>
+      </span>
+      <Headless.ComboboxOptions
+        transition
+        anchor={anchor}
+        className={clsx(
+          // Anchor positioning
+          "[--anchor-gap:--spacing(2)] [--anchor-padding:--spacing(4)] sm:data-[anchor~=start]:[--anchor-offset:-4px]",
+          // Base styles,
+          "isolate min-w-[calc(var(--input-width)+8px)] scroll-py-1 rounded-xl p-1 select-none empty:invisible",
+          // Invisible border that is only visible in `forced-colors` mode for accessibility purposes
+          "outline outline-transparent focus:outline-hidden",
+          // Handle scrolling when menu won't fit in viewport
+          "overflow-y-scroll overscroll-contain",
+          // Popover background
+          "bg-white/75 backdrop-blur-xl dark:bg-zinc-800/75",
+          // Shadows
+          "shadow-lg ring-1 ring-zinc-950/10 dark:ring-white/10 dark:ring-inset",
+          // Transitions
+          "transition-opacity duration-100 ease-in data-closed:data-leave:opacity-0 data-transition:pointer-events-none",
+        )}
+      >
+        {({ option }) => children(option)}
+      </Headless.ComboboxOptions>
+    </Headless.Combobox>
   );
 }
 
@@ -189,40 +144,40 @@ export function ComboboxOption<T>({
   children,
   className,
   ...props
-}: { children?: React.ReactNode } & HeadlessComboboxOptionProps<"div", T>) {
+}: { className?: string; children?: React.ReactNode } & Omit<
+  Headless.ComboboxOptionProps<"div", T>,
+  "as" | "className"
+>) {
   const sharedClasses = clsx(
     // Base
     "flex min-w-0 items-center",
-
     // Icons
-    "*:data-[slot=icon]:size-5 *:data-[slot=icon]:shrink-0 *:data-[slot=icon]:text-zinc-500 group-data-focus/option:*:data-[slot=icon]:text-white sm:*:data-[slot=icon]:size-4 forced-colors:*:data-[slot=icon]:text-[CanvasText] forced-colors:group-data-focus/option:*:data-[slot=icon]:text-[Canvas]",
-
+    "*:data-[slot=icon]:size-5 *:data-[slot=icon]:shrink-0 sm:*:data-[slot=icon]:size-4",
+    "*:data-[slot=icon]:text-zinc-500 group-data-focus/option:*:data-[slot=icon]:text-white dark:*:data-[slot=icon]:text-zinc-400",
+    "forced-colors:*:data-[slot=icon]:text-[CanvasText] forced-colors:group-data-focus/option:*:data-[slot=icon]:text-[Canvas]",
     // Avatars
-    "*:data-[slot=avatar]:size-6 sm:*:data-[slot=avatar]:size-5",
+    "*:data-[slot=avatar]:-mx-0.5 *:data-[slot=avatar]:size-6 sm:*:data-[slot=avatar]:size-5",
   );
 
   return (
-    <HeadlessComboboxOption
-      as="div"
+    <Headless.ComboboxOption
       {...props}
       className={clsx(
         // Basic layout
-        "group/option w-full grid cursor-default grid-cols-[--spacing(5)_1fr] items-baseline gap-x-2 rounded-lg py-2.5 pl-2 pr-3.5 sm:grid-cols-[--spacing(4)_1fr] sm:py-1.5 sm:pl-1.5 sm:pr-3",
+        "group/option grid w-full cursor-default grid-cols-[1fr_--spacing(5)] items-baseline gap-x-2 rounded-lg py-2.5 pr-2 pl-3.5 sm:grid-cols-[1fr_--spacing(4)] sm:py-1.5 sm:pr-2 sm:pl-3",
         // Typography
         "text-base/6 text-zinc-950 sm:text-sm/6 dark:text-white forced-colors:text-[CanvasText]",
-
         // Focus
         "outline-hidden data-focus:bg-blue-500 data-focus:text-white",
-
         // Forced colors mode
         "forced-color-adjust-none forced-colors:data-focus:bg-[Highlight] forced-colors:data-focus:text-[HighlightText]",
-
         // Disabled
         "data-disabled:opacity-50",
       )}
     >
+      <span className={clsx(className, sharedClasses)}>{children}</span>
       <svg
-        className="hidden group-data-selected/option:inline relative self-center stroke-current size-5 sm:size-4"
+        className="hidden group-data-selected/option:inline relative self-center col-start-2 stroke-current size-5 sm:size-4"
         viewBox="0 0 16 16"
         fill="none"
         aria-hidden="true"
@@ -234,10 +189,7 @@ export function ComboboxOption<T>({
           strokeLinejoin="round"
         />
       </svg>
-      <span className={clsx(className, sharedClasses, "col-start-2")}>
-        {children}
-      </span>
-    </HeadlessComboboxOption>
+    </Headless.ComboboxOption>
   );
 }
 
@@ -247,11 +199,11 @@ export function ComboboxLabel({
 }: React.ComponentPropsWithoutRef<"span">) {
   return (
     <span
+      {...props}
       className={clsx(
         className,
         "ml-2.5 truncate first:ml-0 sm:ml-2 sm:first:ml-0",
       )}
-      {...props}
     />
   );
 }
@@ -263,11 +215,11 @@ export function ComboboxDescription({
 }: React.ComponentPropsWithoutRef<"span">) {
   return (
     <span
+      {...props}
       className={clsx(
         className,
-        "flex flex-1 overflow-hidden text-zinc-500 before:w-2 before:min-w-0 before:shrink group-data-focus/option:text-white dark:text-zinc-400",
+        "flex flex-1 overflow-hidden text-zinc-500 group-data-focus/option:text-white before:w-2 before:min-w-0 before:shrink dark:text-zinc-400",
       )}
-      {...props}
     >
       <span className="flex-1 truncate">{children}</span>
     </span>
