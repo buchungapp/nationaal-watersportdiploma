fa32046c515d9bc5cd47854c2fb6bcf9d55ae7de
Maurits Misana <maurits@techmaus.nl>
2025-07-08 21:40:44 +0200

    fix user selector on pvb page

---
+++ apps/web/src/app/(dashboard)/(account)/@selector/profiel/[handle]/pvb-aanvraag/[pvbHandle]/page.tsx
@@ -0,0 +1,30 @@
+import { ChevronDownIcon, UsersIcon } from "@heroicons/react/16/solid";
+import { Badge } from "~/app/(dashboard)/_components/badge";
+import { DropdownButton } from "~/app/(dashboard)/_components/dropdown";
+import { NavbarItem, NavbarLabel } from "~/app/(dashboard)/_components/navbar";
+import { getPersonByHandle } from "~/lib/nwd";
+import { getUserOrThrow } from "~/lib/nwd";
+
+export default async function PersonSelector(props: {
+  params: Promise<{ handle: string }>;
+}) {
+  const { persons } = await getUserOrThrow();
+  const params = await props.params;
+  const person = await getPersonByHandle(params.handle);
+
+  return (
+    <DropdownButton
+      as={NavbarItem}
+      className="[&>*]:bg-white [&>*]:border [&>*]:border-slate-200"
+    >
+      <UsersIcon className="hidden md:block" />
+      <NavbarLabel className="max-w-[80px] truncate">
+        {[person.firstName, person.lastNamePrefix, person.lastName]
+          .filter(Boolean)
+          .join(" ")}
+      </NavbarLabel>
+      <Badge className="-my-1 invisible md:visible">{persons.length}</Badge>
+      <ChevronDownIcon />
+    </DropdownButton>
+  );
+}
